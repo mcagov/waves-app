cd4ad913ee3189aaa24404d818441c4626dd7fc2
Bruno Antunes <bruno@thoughtbot.com>
2016-05-17 16:16:38 +0100

    redo mandatory fields on Registration

--- db/migrate/20160516135205_add_registrations.rb
+++ db/migrate/20160516135205_add_registrations.rb
@@ -1,14 +1,14 @@
 class AddRegistrations < ActiveRecord::Migration
   def change
     create_table :registrations do |t|
-      t.string :ip_country, null: false
-      t.string :card_country, null: false
+      t.string :ip_country
+      t.string :card_country
       t.string :browser, null: false
-      t.string :payment_id, null: false
+      t.string :payment_id
       t.string :receipt_id
       t.string :status, null: false
-      t.datetime :due_date, null: false
-      t.boolean :is_urgent, null: false
+      t.datetime :due_date
+      t.boolean :is_urgent
 
       t.timestamps null: false
     end
