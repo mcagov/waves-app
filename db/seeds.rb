1fd52a05becf5e39733123ea9e4e54f1dcd74dea
Bruno Antunes <bruno@thoughtbot.com>
2016-05-17 14:56:51 +0100

    adds seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# The data can then be loaded with the rake db:seed
-# (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+REGISTERS = ["Part 1", "Part 2", "Part 3", "Part 4"].freeze
+VESSEL_TYPES = [
+  "barge",
+  "dinghy",
+  "hovercraft",
+  "inflatable",
+  "motor sailer",
+  "motor yacht",
+  "narrow boat",
+  "other",
+  "sailing yacht",
+  "sports boat",
+  "wet bike",
+].freeze
+
+REGISTERS.each { |register| Register.find_or_create_by(name: register) }
+
+VESSEL_TYPES.each { |type| VesselType.find_or_create_by(designation: type) }
