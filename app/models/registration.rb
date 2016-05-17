a8f0cc9e3f8820f303d8a889d2c0612336e88bec
Murtaza Gulamali <murtaza@thoughtbot.com>
2016-05-17 17:32:56 +0100

    Enable nested attributes for vessels

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,6 +1,7 @@
 class Registration < ActiveRecord::Base
   has_many :vessel_registrations
   has_many :vessels, through: :vessel_registrations
+  accepts_nested_attributes_for :vessels
 
   validates :not_registered_before_on_ssr, acceptance: true
   validates :not_registered_under_part_1, acceptance: true
