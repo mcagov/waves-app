90f3b29f80bec9414851f10810f29103e6647453
Murtaza Gulamali <murtaza@thoughtbot.com>
2016-06-06 12:38:33 +0100

    Add validations to owner model

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,7 +1,26 @@
 class Owner < ActiveRecord::Base
+  ALLOWED_NATIONALITIES = [
+    "UK"
+  ].freeze
+
   has_many :owner_addresses
   has_many :addresses, through: :owner_addresses
 
   has_many :owner_vessels
   has_many :vessels, through: :owner_vessels
+
+  validates :first_name, presence: true
+
+  validates :last_name, presence: true
+
+  validates(
+    :nationality,
+    presence: true,
+    format: { with: /\A[A-Z]{2}\z/ },
+    inclusion: { in: ALLOWED_NATIONALITIES }
+  )
+
+  validates :email, presence: true
+
+  validates :mobile_number, presence: true
 end
---
+++ spec/factories/owner.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :owner do
+    title       { %w(Miss Mr Mrs Ms).sample }
+    first_name  { %w(Edward Horatio Jack Walter).sample }
+    last_name   { %w(McCallister Nelson Raleigh Smith Sparrow).sample }
+    nationality { Owner::ALLOWED_NATIONALITIES.sample }
+
+    sequence(:email) do |n|
+      "#{first_name}.#{last_name}.#{n}@example.com".downcase
+    end
+
+    mobile_number { "07#{rand.to_s[2..10]}" }
+  end
+end
