beb0504c0ab2808c9b1e08eebabadb0d5154d943
Bruno Antunes <bruno@thoughtbot.com>
2016-06-07 18:20:09 +0100

    WIP

--- db/schema.rb
+++ db/schema.rb
@@ -145,4 +145,6 @@ ActiveRecord::Schema.define(version: 20160609115347) do
     t.string   "radio_call_sign",       null: false
   end
 
+  add_index "vessels", ["vessel_type_id"], name: "index_vessels_on_vessel_type_id", using: :btree
+
 end
