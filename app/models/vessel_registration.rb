0e97ffb907c3d6514dea4115cf93b9b31b33ff44
Murtaza Gulamali <murtaza@thoughtbot.com>
2016-06-09 12:51:20 +0100

    Remove VesselRegistration

--- app/models/vessel_registration.rb
+++
@@ -1,7 +0,0 @@
-class VesselRegistration < ActiveRecord::Base
-  belongs_to :vessel
-  belongs_to :registration
-
-  has_many :user_vessel_registrations
-  has_many :users, through: :user_vessel_registrations
-end
