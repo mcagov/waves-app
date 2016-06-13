b9fedd19621003872dea0b177c7e230b45093cb4
Murtaza Gulamali <murtaza@thoughtbot.com>
2016-06-13 13:06:48 +0100

    Refactor specs

---
+++ spec/support/forms/declaration_form_helper.rb
@@ -0,0 +1,15 @@
+def default_declaration_form_fields
+  {
+    eligible_under_regulation_89: true,
+    eligible_under_regulation_90: true,
+    understands_false_statement_is_offence: true,
+  }.freeze
+end
+
+def complete_declaration_form(fields = default_declaration_form_fields)
+  fill_form_and_submit(
+    :registration,
+    :update,
+    fields
+  )
+end
