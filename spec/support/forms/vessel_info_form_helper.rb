68038c4126c79b29e2ca0e72c54be5cc070ec520
Murtaza Gulamali <murtaza@thoughtbot.com>
2016-06-16 12:25:23 +0100

    Refactor spec helpers

--- spec/support/forms/vessel_info_form_helper.rb
+++ spec/support/forms/vessel_info_form_helper.rb
@@ -33,9 +33,5 @@ def complete_vessel_info_form(fields = default_vessel_info_form_fields)
   complete_vessel_hin_field(fields)
   complete_vessel_length_in_centimeters_field(fields)
 
-  fill_form_and_submit(
-    :vessel,
-    :update,
-    fields
-  )
+  fill_form_and_submit(:vessel, :update, fields)
 end
