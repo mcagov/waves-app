f2e0979661e247ecdee06cf826e6f25d1e9c2ca4
Bruno Antunes <bruno@thoughtbot.com>
2016-06-23 14:49:00 +0100

    change storage for all wizard controllers to "cookies"

--- app/controllers/registration/base_controller.rb
+++ app/controllers/registration/base_controller.rb
@@ -2,14 +2,11 @@ class Registration::BaseController < ApplicationController
   private
 
   def store_in_session(step, params)
-    session[:last_step] = step
-    session[step] = params
-
-    #byebug
+    cookies[step] = params.to_json
   end
 
   def get_from_session(key)
     klass_name = key.to_s.camelize
-    klass_name.constantize.new(session[key])
+    klass_name.constantize.new(JSON.parse(cookies[key]))
   end
 end
