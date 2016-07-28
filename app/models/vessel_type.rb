76cdea1fe1a3bb53a87788eb63b2d8d89a57e784
Toby Privett <toby@snaplab.co.uk>
2016-07-28 13:48:58 +0100

    Remove unnecessary associations
    - Unlikely we will need to list all vessels of
    a given type

--- app/models/vessel_type.rb
+++ app/models/vessel_type.rb
@@ -1,17 +1,2 @@
 class VesselType < ApplicationRecord
-  has_many :vessels
-
-  def self.collected_designations
-    other_vessel_type = VesselType.find_by(designation: "other")
-
-    vessels =
-      VesselType.all.each_with_object([]) do |vessel_type, memo|
-        next if vessel_type.id == other_vessel_type.id
-
-        memo << [vessel_type.designation.titleize, vessel_type.id]
-      end
-
-    vessels << [("&#8212;" * 10).html_safe, { disabled: true }]
-    vessels << ["Other (please specify)", other_vessel_type.id]
-  end
 end
