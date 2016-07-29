5999a42dbe7419f5b2be75a4a8ee4da3d3b53a50
Toby Privett <toby@snaplab.co.uk>
2016-07-29 16:28:57 +0100

    Migrate the design stylesheets and layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
-  layout "public"
+  layout :determine_layout
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def determine_layout
+    current_user ? "private" : "application"
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,3 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
-
-  layout :determine_layout
-
-  private
-
-  def determine_layout
-    case params[:id]
-    when "start"
-      "public"
-    else
-      "private"
-    end
-  end
 end
