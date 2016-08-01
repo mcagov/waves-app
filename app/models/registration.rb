2445707c217a115a6843eff14d862bdb5e87deed
Toby Privett <toby@snaplab.co.uk>
2016-08-01 13:03:59 +0100

    Disable nested attributes for a registration's vessel and delivery address

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,7 +1,5 @@
 class Registration < ApplicationRecord
   belongs_to :vessel
-  accepts_nested_attributes_for :vessel
 
   belongs_to :delivery_address, class_name: "Address"
-  accepts_nested_attributes_for :delivery_address
 end
