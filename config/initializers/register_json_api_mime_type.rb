a3df98f289075f400dc19758c5ecac5615d7bd62
Toby Privett <toby@snaplab.co.uk>
2016-08-04 11:09:53 +0100

    Add json_api mime type to support json_api requests

---
+++ config/initializers/register_json_api_mime_type.rb
@@ -0,0 +1,12 @@
+# Here we set the mime type so that the api can handle
+# requests that conform to the jsonapi.org spec
+# Thanks to: https://github.com/rails-api/active_model_serializers/issues/1027
+
+api_mime_types = %W(
+  application/vnd.api+json
+  text/x-json
+  application/json
+)
+
+Mime::Type.unregister :json
+Mime::Type.register 'application/json', :json, api_mime_types
