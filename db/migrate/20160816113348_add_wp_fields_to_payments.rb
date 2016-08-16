d2b9ec92d17ef10b5a70ae5ed1b83aa48f78cdb2
Toby Privett <toby@snaplab.co.uk>
2016-08-16 14:32:51 +0100

    Payments table should have UUID keys

--- db/migrate/20160816113348_add_wp_fields_to_payments.rb
+++ db/migrate/20160816113348_add_wp_fields_to_payments.rb
@@ -1,6 +1,6 @@
 class AddWpFieldsToPayments < ActiveRecord::Migration[5.0]
   def change
-    remove_column :payments, :changeset
+    remove_column :payments, :changeset, :string
     add_column :payments, :wp_token, :string
     add_column :payments, :wp_order_code, :string
     add_column :payments, :wp_amount, :string
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160816113348) do
+ActiveRecord::Schema.define(version: 20160816132951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,8 +54,8 @@ ActiveRecord::Schema.define(version: 20160816113348) do
     t.uuid     "address_id"
   end
 
-  create_table "payments", force: :cascade do |t|
-    t.integer  "registration_id"
+  create_table "payments", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.uuid     "registration_id"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
     t.string   "wp_token"
