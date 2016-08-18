3c366acf5611762dd2ec4b8990678506da4c1ef9
Toby Privett <toby@snaplab.co.uk>
2016-08-18 17:15:49 +0100

    Submission::Vessel should not be an ActiveRecord object
    - It was appealing to init the submission::vessel as
    a subclass of vessel, but there was something
    weird going on and the object wasn’t always
    ready when the page loads

--- app/models/submission/vessel.rb
+++ app/models/submission/vessel.rb
@@ -1,5 +1,12 @@
-class Submission::Vessel < Vessel
+class Submission::Vessel
   attr_accessor(
+    :name,
+    :hin,
+    :make_and_model,
+    :length_in_meters,
+    :number_of_hulls,
+    :mmsi_number,
+    :radio_call_sign,
     :alt_name_1,
     :alt_name_2,
     :alt_name_3,
@@ -10,7 +17,7 @@ class Submission::Vessel < Vessel
 
   def initialize(params={})
     params.reject!{|param| !self.respond_to?(param)}
-    super
+    params.each { |key,value| instance_variable_set("@#{key}", value) }
   end
 
   def type_of_vessel
