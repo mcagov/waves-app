cfb732139cdaf4014ea5d547319998ae2ae67bca
Toby Privett <toby@snaplab.co.uk>
2016-08-18 08:54:02 +0100

    wip toggle

--- spec/features/user_toggles_sidebar_spec.rb
+++ spec/features/user_toggles_sidebar_spec.rb
@@ -1,5 +1,24 @@
 require "rails_helper"
 
-feature "User toggles the sidebar", type: :feature do
-  scenario "it writes this test"
+feature "User toggles the sidebar", type: :feature, javascript: true do
+  before { login }
+
+  scenario do
+    visit "/"
+    save_and_open_page
+    click_link('My Tasks')
+    expect(page).to have_css('h1', 'My Tasks')
+
+    click_link('Unclaimed Tasks')
+    expect(page).to have_css('h1', 'Unclaimed Tasks')
+
+    click_link('Print Queue')
+    expect(page).to have_css('h1', 'Print Queue')
+
+    click_link('Referred Applications')
+    expect(page).to have_css('h1', 'Referred Applications')
+
+    click_link('Incomplete Applications')
+    expect(page).to have_css('h1', 'Incomplete Applications')
+  end
 end
