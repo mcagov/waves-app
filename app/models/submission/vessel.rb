cad9207a09bc6cd5f99efd578a67218921d4e1ae
Toby Privett <toby@snaplab.co.uk>
2016-08-18 16:03:18 +0100

    Display vessel type as #type_of_vessel
    - Here we can display the type of vessel
    without caring if the underlying data is 
    vessel_type or vessel_type_other

--- app/models/submission/vessel.rb
+++ app/models/submission/vessel.rb
@@ -5,11 +5,20 @@ class Submission::Vessel < Vessel
     :alt_name_3,
     :length_in_meters,
     :vessel_type,
+    :vessel_type_other,
     :owners
   )
 
   def initialize(params={})
     params.reject!{|param| !self.respond_to?(param)}
     super
+  end
+
+  def type_of_vessel
+    if vessel_type_other.present?
+      vessel_type_other
+    else
+      vessel_type
+    end
   end
 end
--- spec/models/submission/vessel_spec.rb
+++ spec/models/submission/vessel_spec.rb
@@ -2,21 +2,52 @@ require "rails_helper"
 
 describe Submission::Vessel, type: :model do
   context "#new" do
-    let(:input_params) do
-      {
-        name: "A real name",
-        non_existent: "rubbish"
-      }
-    end
-
     subject { Submission::Vessel.new(input_params)}
 
-    it "has a name" do
-      expect(subject.name).to eq("A real name")
+    context "in general" do
+      let(:input_params) do
+        {
+          name: "A real name",
+          non_existent: "rubbish"
+        }
+      end
+
+      it "has a name" do
+        expect(subject.name).to eq("A real name")
+      end
+
+      it "does not have non_existent" do
+        expect{ subject.non_existent }.to raise_error(NoMethodError)
+      end
     end
 
-    it "does not have non_existent" do
-      expect{ subject.non_existent }.to raise_error(NoMethodError)
+
+    context "#type_of_vessel" do
+      context "when vessel_type was chosen" do
+        let(:input_params) do
+          {
+            vessel_type: "Barge",
+            vessel_type_other: ""
+          }
+        end
+
+        it "uses the vessel_type field" do
+          expect(subject.type_of_vessel).to eq("Barge")
+        end
+      end
+
+      context "when vessel_type_other was chosen" do
+        let(:input_params) do
+          {
+            vessel_type: "?",
+            vessel_type_other: "Beer mug"
+          }
+        end
+
+        it "uses the vessel_type_other field" do
+          expect(subject.type_of_vessel).to eq("Beer mug")
+        end
+      end
     end
   end
 end
