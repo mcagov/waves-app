78f071f55b7b6ee52c0ad4951f9b480143f27403
Toby Privett <toby@snaplab.co.uk>
2016-08-19 14:11:00 +0100

    Display transition-based lists in the tasks pages

--- spec/features/user_views_dashboard_spec.rb
+++ spec/features/user_views_dashboard_spec.rb
@@ -1,18 +1,18 @@
 require "rails_helper"
 
 feature "User views dashboard", type: :feature, js: true do
-  let!(:submission) { create_submission! }
+  let!(:submission) { create_paid_submission! }
 
   before do
     login
   end
 
-  scenario "submissions" do
+  scenario "viewing the unclaimed submissions" do
     within("#submissions") do
       expect(page).to have_content("Celebrator Doppelbock")
       expect(page).to have_content("Horatio Nelson")
       expect(page).to have_content("New Registration")
-      expect(page).to have_css(".fa-times.i.red")
+      expect(page).to have_css(".fa-check.i.green")
       expect(page).to have_content(submission.target_date)
       expect(page).to have_content(submission.source)
     end
