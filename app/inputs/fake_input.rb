8a041a2f09d96262c93007939265552618dbe10b
Toby Privett <toby@snaplab.co.uk>
2016-08-19 10:36:09 +0100

    Remove unused input formatters

--- app/inputs/fake_input.rb
+++
@@ -1,8 +0,0 @@
-class FakeInput < BaseInput
-  def input(wrapper_options = nil)
-    merged_options = merge_wrapper_options(input_html_options, wrapper_options)
-    name = "#{object_name}[#{attribute_name}]"
-
-    template.text_field_tag(name, nil, merged_options)
-  end
-end
