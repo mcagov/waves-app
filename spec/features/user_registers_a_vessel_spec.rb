ffb0089ec34f12a95d122a63b9401b4eb4a0d801
Toby Privett <toby@snaplab.co.uk>
2016-08-22 15:31:37 +0100

    Ensure that a completed submission is not visible

--- spec/features/user_registers_a_vessel_spec.rb
+++ spec/features/user_registers_a_vessel_spec.rb
@@ -14,5 +14,8 @@ feature "User registers a vessel", type: :feature, js: true do
     within('.modal-content') { click_button('Register Vessel') }
 
     expect(page).to have_text("vessel is now registered")
+
+    visit tasks_my_tasks_path
+    expect(page).not_to have_css('tr.submission')
   end
 end
