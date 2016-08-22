95aa03c6107b86b7478ea99a032bbb06861062a1
Toby Privett <toby@snaplab.co.uk>
2016-08-22 13:57:32 +0100

    Remove vessel_type_other from vessels

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822124745) do
+ActiveRecord::Schema.define(version: 20160822125212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,7 +91,6 @@ ActiveRecord::Schema.define(version: 20160822124745) do
     t.integer  "number_of_hulls",       null: false
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
-    t.string   "vessel_type_other"
     t.integer  "mmsi_number",           null: false
     t.string   "radio_call_sign",       null: false
     t.string   "vessel_type"
