e33c5b19317079e0cca6fc566c43728bbddbb56a
Toby Privett <toby@snaplab.co.uk>
2016-08-30 14:06:34 +0100

    Tidy up the submission model with a concerns mixin

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,6 @@
 class Submission < ApplicationRecord
+  include SubmissionTransitions
+
   belongs_to :delivery_address, class_name: "Address", required: false
   belongs_to :claimant, class_name: "User", required: false
 
@@ -15,51 +17,11 @@ class Submission < ApplicationRecord
   validates :ref_no, presence: true
   before_validation :set_ref_no
 
-  include ActiveModel::Transitions
-
-  state_machine auto_scopes: true do
-    state :incomplete
-    state :unassigned
-    state :assigned
-    state :referred
-    state :completed
-    state :rejected
-    state :referred
-
-    event :paid do
-      transitions to: :unassigned, from: :incomplete,
-        on_transition: :set_target_date_and_urgent_flag
-    end
-
-    event :claimed do
-      transitions to: :assigned, from: [:unassigned, :rejected, :referred]
-    end
-
-    event :unclaimed do
-      transitions to: :unassigned, from: :assigned,
-        on_transition: :remove_claimant
-    end
-
-    event :approved do
-      transitions to: :completed, from: :assigned
-    end
-
-    event :rejected do
-      transitions to: :rejected, from: :assigned,
-        on_transition: :remove_claimant
-    end
-
-    event :referred do
-      transitions to: :referred, from: :assigned,
-        on_transition: :remove_claimant
-    end
+  def set_ref_no
+    self.ref_no ||= RefNo.generate("00")
   end
 
   def process_application; end
-
-  def reference_no
-    "--Pending--"
-  end
 
   def paid?
     payment.present?
@@ -97,22 +59,5 @@ class Submission < ApplicationRecord
   def user_input
     @user_input ||=
       changeset.blank? ? {} : changeset.deep_symbolize_keys!
-  end
-
-  def remove_claimant
-    update_attribute(:claimant, nil)
-  end
-
-  def set_target_date_and_urgent_flag
-    if paid? && payment.wp_amount.to_i == 7500
-      update_attribute(:target_date, 5.days.from_now)
-      update_attribute(:is_urgent, true)
-    else
-      update_attribute(:target_date, 20.days.from_now)
-    end
-  end
-
-  def set_ref_no
-    self.ref_no ||= RefNo.generate("00")
   end
 end
