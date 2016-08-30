ce2be49b58b02c7e9fdb4fe2fd27d43a4998d1f8
Toby Privett <toby@snaplab.co.uk>
2016-08-30 16:11:21 +0100

    Move Reclaim button to submission page (for referral)

--- spec/features/user_refers_submission_spec.rb
+++ spec/features/user_refers_submission_spec.rb
@@ -15,10 +15,8 @@ feature "User refers a submission", type: :feature, js: true do
     within('#refer-application') { click_on "Refer Application" }
 
     click_on "Referred Applications"
-    expect(page).to have_css('.referred-until', text: "12/12/2020")
-
     click_on("Celebrator Doppelbock")
-    expect(page).to have_css('#prompt', /Application Referred by.*: Unsuitable name. Next action .*/)
+    expect(page).to have_css('#prompt', /Application Referred by.*: Unsuitable name. Next action 12\/12\/2020/)
 
     click_on "Cancel Referral"
 
