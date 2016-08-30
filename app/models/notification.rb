911991e22aaac2f682884b1a313258a915a62f8e
Toby Privett <toby@snaplab.co.uk>
2016-08-30 16:03:32 +0100

    Save actioned_by when creating a notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,6 @@
 class Notification < ApplicationRecord
   belongs_to :submission
+  belongs_to :actioned_by, class_name: "User"
 
   # while the due_by date *belongs* in the Notification::Referral model
   # it is here so we can create a Notification in the modal without
