db5ce6cf04b0b6e6769f0b3e7c82bf3e0164d4ae
Toby Privett <toby@snaplab.co.uk>
2016-09-07 13:26:11 +0100

    Hot fix: test_email params

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationMailer < ApplicationMailer
   default from: ENV.fetch("EMAIL_FROM")
 
-  def test_email(email: "test@example.com")
+  def test_email(email)
     mail(to: email, subject: "MCA test email service")
   end
 end
