194a7f1f75d3f75746daaa256e8b740e32d4f6e8
Toby Privett <toby@snaplab.co.uk>
2016-09-07 14:29:41 +0100

    Notification does not need state transitions
    - We can handle delivery with a simple after_create
    callback

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,17 +2,7 @@ class Notification < ApplicationRecord
   belongs_to :submission
   belongs_to :actioned_by, class_name: "User"
 
-  include ActiveModel::Transitions
-
-  state_machine auto_scopes: true do
-    state :queued
-    state :delivered
-
-    event :deliver do
-      transitions to: :delivered, from: :queued,
-                  on_transition: :send_email
-    end
-  end
+  after_create :send_email
 
   def send_email
     NotificationMailer.delay.test_email("test@example.com")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907103613) do
+ActiveRecord::Schema.define(version: 20160907132652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,8 +98,6 @@ ActiveRecord::Schema.define(version: 20160907103613) do
     t.uuid     "actioned_by_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-    t.string   "state"
-    t.index ["state"], name: "index_notifications_on_state", using: :btree
     t.index ["type"], name: "index_notifications_on_type", using: :btree
   end
 
