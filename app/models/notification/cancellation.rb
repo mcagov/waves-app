058d442bbc544f6a6b7906d6686e56c077fe6b24
Toby Privett <toby@snaplab.co.uk>
2016-09-08 10:43:11 +0100

    Symbolise cancellation reasons
    - We will need to make decisions based on the
    subject value, so this should be baked into the model

--- app/models/notification/cancellation.rb
+++ app/models/notification/cancellation.rb
@@ -1,2 +1,14 @@
 class Notification::Cancellation < Notification
+  REASONS = [
+    :owner_request,
+    :no_response_from_owner].freeze
+
+  def email_template
+    case subject
+    when :owner_request
+      :cancellation_owner_request
+    when :no_reponse_from_owner
+      :cancellation_no_response
+    end
+  end
 end
