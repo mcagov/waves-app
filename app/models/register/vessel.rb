61710ff4319f1eac7c17d236d655314a0e2088b6
Toby Privett <toby@snaplab.co.uk>
2016-09-09 09:17:16 +0100

    Removing legacy vessel validations
    - This will remain in govuk

--- app/models/register/vessel.rb
+++ app/models/register/vessel.rb
@@ -1,16 +1,5 @@
 module Register
   class Vessel < ApplicationRecord
-    include VesselValidations
     has_many :registered_owners, class_name: "Register::Owner"
-
-    private
-
-    def hin_must_begin_with_a_valid_country_code
-      return if hin.blank? || ISO3166::Data.codes.include?(hin[0..1])
-
-      message =
-        I18n.t("activerecord.errors.models.vessel.attributes.hin.invalid")
-      errors.add(:hin, message)
-    end
   end
 end
