641aa78b437306cd6c88b7697a2c846de8bef46a
Toby Privett <toby.privett@oceanshq.com>
2016-09-13 14:13:31 +0100

    Format the history date

--- config/initializers/date_format.rb
+++ config/initializers/date_format.rb
@@ -1,3 +1,3 @@
 Date::DATE_FORMATS[:default] = "%d/%m/%Y"
 Time::DATE_FORMATS[:default] = "%d/%m/%Y"
-Time::DATE_FORMATS[:date_time] = "%a %b, %Y at %H:%M"
+Time::DATE_FORMATS[:date_time] = "%a %b %d, %Y at %H:%M"
