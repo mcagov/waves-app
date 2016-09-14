9b83c7b25960d147865f49966ed0f5eb12c46d0e
Toby Privett <toby.privett@oceanshq.com>
2016-09-14 17:15:24 +0100

    Build submission#notification_list in the model
    - Initially, I thought that this could be handled
    in a view helper, but as other notification
    types pop up, a helper will get messy

--- spec/models/submission_spec.rb
+++ spec/models/submission_spec.rb
@@ -140,4 +140,34 @@ describe Submission, type: :model do
       it { expect(submissions).to be_empty }
     end
   end
+
+  context "#notification_list" do
+    let!(:submission) { create(:submission) }
+
+    let!(:outstanding_declaration) do
+      create(
+        :notification,
+        notifiable: create(:declaration, submission: submission),
+        created_at: Date.today)
+    end
+
+    let!(:old_notification) do
+      create(:notification, notifiable: submission, created_at: 1.year.ago)
+    end
+
+    let!(:recent_notification) do
+      create(:notification, notifiable: submission, created_at: 1.day.ago)
+    end
+
+    let!(:correspondence) do
+      create(:correspondence, noteable: submission, created_at: 3.days.ago)
+    end
+
+    it "builds a list" do
+      expect(submission.notification_list).to eq(
+        [outstanding_declaration,
+         recent_notification, correspondence, old_notification]
+      )
+    end
+  end
 end
