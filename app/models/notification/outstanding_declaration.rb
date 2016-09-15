66fb5a0084c632c1a89a3b01ccba4203d2b56a76
Toby Privett <toby.privett@oceanshq.com>
2016-09-15 11:15:07 +0100

    Applicant and vessel names for declaration email

--- app/models/notification/outstanding_declaration.rb
+++ app/models/notification/outstanding_declaration.rb
@@ -4,10 +4,20 @@ class Notification::OutstandingDeclaration < Notification
   end
 
   def additional_params
-    notifiable.id
+    [notifiable.id, vessel_name]
   end
 
   def email_subject
     "Vessel Registration Owner Declaration Required"
+  end
+
+  private
+
+  def vessel_name
+    notifiable.submission.vessel  if notifiable.submission
+  end
+
+  def applicant_name
+    notifiable.applicant_name if notifiable
   end
 end
