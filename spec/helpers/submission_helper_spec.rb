b84633f15aa559dd366e97aca921569d982b70c1
Toby Privett <toby.privett@oceanshq.com>
2016-09-16 13:59:05 +0100

    Helper method #similar_attribute icon

--- spec/helpers/submission_helper_spec.rb
+++ spec/helpers/submission_helper_spec.rb
@@ -11,4 +11,21 @@ describe "SubmissionHelper", type: :helper do
       expect(helper.css_tick(false)).to match(/i fa fa-times red/)
     end
   end
+
+  describe "#similar_attribute_icon", type: :helper do
+    it "shows the icon" do
+      expect(helper.similar_attribute_icon("a", "a"))
+        .to match(/i fa fa-star-o/)
+    end
+
+    it "does not show the icon with different values" do
+      expect(helper.similar_attribute_icon("a", 1))
+        .to be_blank
+    end
+
+    it "does not show the icon with empty strings" do
+      expect(helper.similar_attribute_icon("", ""))
+        .to be_blank
+    end
+  end
 end
