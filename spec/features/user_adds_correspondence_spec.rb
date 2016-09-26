b5dc8676c7240084da0494d90d9f089bcd1dc368
Toby Privett <toby.privett@oceanshq.com>
2016-09-26 12:09:33 +0100

    Extract submission#notification_list to shared
    - Notification list functionality will be used by
    both submissions and vessels

--- spec/features/user_adds_correspondence_spec.rb
+++ spec/features/user_adds_correspondence_spec.rb
@@ -1,11 +1,26 @@
 require "rails_helper"
 
 feature "User adds correspondence", type: :feature, js: true do
-  before do
+  scenario "to a submission" do
     visit_assigned_submission
+    click_on("Correspondence")
+    click_link("Add Correspondence")
+
+    fill_in "Subject", with: "My Subject"
+    select "Post", from: "Correspondence Format"
+    fill_in "Date Received", with: "12/12/2015"
+    fill_in "Content", with: "Hello Bob"
+    click_on "Save Correspondence"
+
+    click_on("Correspondence")
+    click_on("My Subject")
+
+    expect(page).to have_css("h4", text: "My Subject")
+    expect(page).to have_css(".modal-body", text: "Hello Bob")
   end
 
-  scenario "to a submission" do
+  xscenario "to a vessel" do
+    visit_registered_vessel
     click_on("Correspondence")
     click_link("Add Correspondence")
 
