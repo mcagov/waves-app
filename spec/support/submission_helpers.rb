f499cf82c44e820249686da8ebd7429da5bf0552
Toby Privett <toby.privett@oceanshq.com>
2016-09-26 11:40:03 +0100

    No need to define the SubmissionHelpers

--- spec/support/submission_helpers.rb
+++ spec/support/submission_helpers.rb
@@ -1,85 +1,83 @@
-module SubmissionHelpers
-  def new_registration_attributes
-    JSON.parse(
-      File.read("spec/fixtures/new_registration.json")
-    )["data"]["attributes"]
-  end
+def new_registration_attributes
+  JSON.parse(
+    File.read("spec/fixtures/new_registration.json")
+  )["data"]["attributes"]
+end
 
-  def create_incomplete_submission!
-    Submission::NewRegistration.create(new_registration_attributes)
-  end
+def create_incomplete_submission!
+  Submission::NewRegistration.create(new_registration_attributes)
+end
 
-  def create_incomplete_paid_submission!
-    submission = create_incomplete_submission!
-    pay_submission(submission)
+def create_incomplete_paid_submission!
+  submission = create_incomplete_submission!
+  pay_submission(submission)
 
-    submission.reload
-  end
+  submission.reload
+end
 
-  def create_unassigned_submission!
-    submission = create_incomplete_submission!
-    pay_submission(submission)
+def create_unassigned_submission!
+  submission = create_incomplete_submission!
+  pay_submission(submission)
 
-    submission.declarations.incomplete.map(&:declared!)
-    submission.reload
-  end
+  submission.declarations.incomplete.map(&:declared!)
+  submission.reload
+end
 
-  def create_unassigned_urgent_submission!
-    submission = create_incomplete_submission!
-    pay_submission(submission, "urgent_payment")
+def create_unassigned_urgent_submission!
+  submission = create_incomplete_submission!
+  pay_submission(submission, "urgent_payment")
 
-    submission.declarations.incomplete.map(&:declared!)
-    submission.reload
-  end
+  submission.declarations.incomplete.map(&:declared!)
+  submission.reload
+end
 
-  def create_assigned_submission!
-    submission = create_unassigned_submission!
+def create_assigned_submission!
+  submission = create_unassigned_submission!
 
-    submission.claimed!(create(:user))
-    submission.reload
-  end
+  submission.claimed!(create(:user))
+  submission.reload
+end
 
-  def create_referred_submission!
-    submission = create_assigned_submission!
+def create_referred_submission!
+  submission = create_assigned_submission!
 
-    submission.referred!
-    submission.reload
-  end
+  submission.referred!
+  submission.reload
+end
 
-  def create_completed_submission!
-    submission = create_assigned_submission!
-    submission.approved!
+def create_completed_submission!
+  submission = create_assigned_submission!
+  submission.approved!
 
-    submission.reload
-  end
+  submission.reload
+end
 
-  def visit_assigned_submission
-    submission = create_assigned_submission!
+def visit_assigned_submission
+  submission = create_assigned_submission!
 
-    # we don't need the second declaration
-    submission.declarations.last.destroy
+  # we don't need the second declaration
+  submission.declarations.last.destroy
 
-    login_to_part_3(submission.claimant)
-    visit submission_path(submission)
-  end
+  login_to_part_3(submission.claimant)
+  visit submission_path(submission)
+end
 
-  def visit_completed_submission
-    submission = create_completed_submission!
+def visit_completed_submission
+  submission = create_completed_submission!
 
-    # we don't need the second declaration
-    submission.declarations.last.destroy
+  # we don't need the second declaration
+  submission.declarations.last.destroy
 
-    login_to_part_3(submission.claimant)
-    visit submission_path(submission)
-  end
+  login_to_part_3(submission.claimant)
+  visit submission_path(submission)
+end
 
-  def pay_submission(submission, payment_file = "new_payment")
-    payment_json =
-      JSON.parse(File.read("spec/fixtures/#{payment_file}.json"))
+def pay_submission(submission, payment_file = "new_payment")
+  payment_json =
+    JSON.parse(File.read("spec/fixtures/#{payment_file}.json"))
 
-    payment = Payment.new(payment_json["data"]["attributes"])
-    payment.update_attribute(:submission_id, submission.id)
-    submission.paid!
-    submission
-  end
+  payment = Payment.new(payment_json["data"]["attributes"])
+  payment.update_attribute(:submission_id, submission.id)
+  submission.paid!
+  submission
 end
