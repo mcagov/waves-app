d17d65d53be3211f3b9f7c5595581fd67509e06c
Toby Privett <toby.privett@oceanshq.com>
2016-09-28 16:11:44 +0100

    Remove state_machine and sms from client session

--- app/models/client_session.rb
+++ app/models/client_session.rb
@@ -9,25 +9,13 @@ class ClientSession < ApplicationRecord
   validates :external_session_key, presence: true
   validates :otp, presence: true, length: { is: 6 }
 
-  include ActiveModel::Transitions
-
-  state_machine auto_scopes: true do
-    state :unused, enter: :init_client_session
-    state :used
+  before_validation(on: :create) do
+    self.otp = rand(10000..999999)
   end
 
   def obfuscated_recipient_phone_numbers
     vessel.owners.map do |owner|
       "######{owner.phone_number.last(3)}"
     end
-  end
-
-  private
-
-  def init_client_session
-    self.otp = rand(10000..999999)
-
-    return unless valid?
-    SmsProvider.send_otp(vessel.owners, otp)
   end
 end
--- spec/models/client_session_spec.rb
+++ spec/models/client_session_spec.rb
@@ -2,15 +2,16 @@ require "rails_helper"
 
 describe ClientSession do
   context "#create" do
+    let!(:vessel) { create(:register_vessel) }
+    let!(:owner) { create(:register_owner, vessel: vessel) }
+
     subject do
       ClientSession.create(
         vessel_reg_no: vessel_reg_no,
         external_session_key: external_session_key)
     end
 
-    context "with a valid vessel reg_no" do
-      let!(:vessel) { create(:register_vessel) }
-      let!(:owner) { create(:register_owner, vessel: vessel) }
+    context "with valid attributes" do
       let(:vessel_reg_no) { vessel.reg_no }
       let(:external_session_key) { "123" }
 
@@ -22,38 +23,26 @@ describe ClientSession do
         expect(subject.otp).to be_present
       end
 
-      it "has an initial_state :unused" do
-        expect(subject).to be_unused
-      end
-
       it "belongs to the vessel" do
         expect(subject.vessel).to eq(vessel)
-      end
-
-      it "delivers a notify message" do
-        expect(Delayed::Job.count).to eq(1)
       end
 
       it "returns the obfuscated_recipient_phone_numbers" do
         expect(subject.obfuscated_recipient_phone_numbers)
           .to eq(["######{owner.phone_number.last(3)}"])
       end
-
-      context "but no external_session_key" do
-        let(:external_session_key) { "" }
-
-        it "has an error on the external_session_key" do
-          expect(subject.errors).to include(:external_session_key)
-        end
-      end
     end
 
-    context "with an invalid vessel reg_no" do
+    context "with invalid attributes" do
       let(:vessel_reg_no) { "foo" }
-      let(:external_session_key) { "123" }
+      let(:external_session_key) { "" }
 
       it "does not belong to a vessel" do
         expect(subject.vessel).to be_nil
+      end
+
+      it "has an error on the external_session_key" do
+        expect(subject.errors).to include(:external_session_key)
       end
     end
   end
