0d52ad3c08c102d793ed1d79cfc167e6e4cfa39b
Toby Privett <toby.privett@oceanshq.com>
2016-09-28 12:19:43 +0100

    Set up the client session with stubbed OTP

--- app/models/client_session.rb
+++ app/models/client_session.rb
@@ -1,2 +1,23 @@
 class ClientSession < ApplicationRecord
+  belongs_to :vessel,
+             foreign_key: :vessel_reg_no,
+             primary_key: :reg_no,
+             class_name: "Register::Vessel"
+
+  validates :vessel_reg_no, presence: true
+  validates :external_session_key, presence: true
+  validates :otp, presence: true, length: { is: 6 }
+
+  include ActiveModel::Transitions
+
+  state_machine auto_scopes: true do
+    state :unused, enter: :init_client_session
+    state :used
+  end
+
+  private
+
+  def init_client_session
+    self.otp = 111111
+  end
 end
---
+++ spec/models/client_session_spec.rb
@@ -0,0 +1,50 @@
+require "rails_helper"
+
+describe ClientSession do
+  context "#create" do
+    subject do
+      ClientSession.create(
+        vessel_reg_no: vessel_reg_no,
+        external_session_key: external_session_key)
+    end
+
+    context "with a valid vessel reg_no" do
+      let!(:vessel) { create(:register_vessel) }
+      let(:vessel_reg_no) { vessel.reg_no }
+      let(:external_session_key) { "123" }
+
+      it "is persisted" do
+        expect(subject).to be_persisted
+      end
+
+      it "sets the OTP" do
+        expect(subject.otp).to be_present
+      end
+
+      it "has an initial_state :unused" do
+        expect(subject).to be_unused
+      end
+
+      it "belongs to the vessel" do
+        expect(subject.vessel).to eq(vessel)
+      end
+
+      context "but no external_session_key" do
+        let(:external_session_key) { "" }
+
+        it "has an error on the external_session_key" do
+          expect(subject.errors).to include(:external_session_key)
+        end
+      end
+    end
+
+    context "with an invalid vessel reg_no" do
+      let(:vessel_reg_no) { "foo" }
+      let(:external_session_key) { "123" }
+
+      it "does not belong to a vessel" do
+        expect(subject.vessel).to be_nil
+      end
+    end
+  end
+end
