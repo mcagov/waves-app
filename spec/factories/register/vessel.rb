990d88cb6ff0f71f385934f83f700253e0c07584
Toby Privett <toby.privett@oceanshq.com>
2016-10-03 15:43:26 +0100

    Add a basic test for the Registration Certificate
    - Here we are only testing that the pdf renders
    and it has the expected filename

--- spec/factories/register/vessel.rb
+++ spec/factories/register/vessel.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :register_vessel, class: "Register::Vessel" do
     sequence(:name)           { |n| "Boaty McBoatface #{n}" }
     number_of_hulls           { rand(1..6) }
+    vessel_type               "BARGE"
   end
 end
