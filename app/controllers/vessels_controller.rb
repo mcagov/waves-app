e06facb21776ff348c711074efe685d70b8541cb
Toby Privett <toby.privett@oceanshq.com>
2016-10-06 10:40:12 +0100

    Remove temp link to print reg certificate

--- app/controllers/vessels_controller.rb
+++ app/controllers/vessels_controller.rb
@@ -1,16 +1,6 @@
 class VesselsController < InternalPagesController
   def show
     @vessel = Register::Vessel.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.pdf do
-        pdf = RegistrationCertificate.new(@vessel)
-        send_data pdf.render, filename: pdf.filename,
-                              type: "application/pdf",
-                              disposition: "inline"
-      end
-    end
   end
 
   def index
