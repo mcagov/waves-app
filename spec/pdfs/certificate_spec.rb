d0f644df84134c993e08c6e693df4c67cde9584a
Toby Privett <toby.privett@oceanshq.com>
2016-10-10 09:08:48 +0100

    Pending specs to remind us to thoroughy test the PDF builders

--- spec/pdfs/certificate_spec.rb
+++ spec/pdfs/certificate_spec.rb
@@ -1,14 +1,14 @@
 require "rails_helper"
 
 describe Certificate do
-  context "in general" do
+  context "for a single registration" do
     let!(:vessel) { create(:register_vessel, name: "Jolly Roger") }
 
     let!(:registration) do
       create(:registration, vessel: vessel, registered_at: "2012-12-03")
     end
 
-    subject { RegistrationCertificate.new(vessel) }
+    subject { Certificate.new(registration) }
 
     it "has a filename" do
       expect(subject.filename).to eq("jolly-roger-registration-2012-12-03.pdf")
@@ -18,6 +18,10 @@ describe Certificate do
       expect(subject.render[0, 4]).to eq("%PDF")
     end
 
-    it "tests the rendering of multiple certificates in one PDF"
+    it "tests the pdf has the expected elements"
+  end
+
+  context "for multiple registrations" do
+    it "has multiple pages"
   end
 end
