0373dec930fd2e242939eedb0ec54c3a10907dbc
Toby Privett <toby.privett@oceanshq.com>
2016-10-14 10:51:21 +0100

    Render the static Record Payment page

--- spec/features/finance_enters_payment_spec.rb
+++ spec/features/finance_enters_payment_spec.rb
@@ -7,11 +7,6 @@ describe "Finance enters a payment" do
 
   scenario "in its simplest form" do
     expect(page).to have_css(".active-register", text: "Active: Finance")
-    expect(page).to have_css("h1", text: "Finance: Payments")
-
-    click_on("New Batch")
-
-
-
+    expect(page).to have_css("h1", text: "Finance: Record Payment")
   end
 end
