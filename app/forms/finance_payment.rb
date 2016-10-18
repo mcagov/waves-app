144403af76843140bde3e4a3f18ef6a6d5cfce4a
Toby Privett <toby.privett@oceanshq.com>
2016-10-18 16:11:44 +0100

    Create a (simple) submission
    - In its simplest form, this submission can
    be viewed in the unclaimed tasks queue

--- app/forms/finance_payment.rb
+++ app/forms/finance_payment.rb
@@ -14,4 +14,51 @@ class FinancePayment
     :applicant_email,
     :documents_received
   )
+
+  def create_submission
+    @finance_payment = Payment.create(
+      amount: payment_amount,
+      submission: build_submission
+    )
+    @finance_payment.submission
+  end
+
+  private
+
+  def build_submission
+    Submission.new(
+      part: part,
+      task: task,
+      source: :manual_entry,
+      is_urgent: (service_level != :standard),
+      changeset: build_changeset
+    )
+  end
+
+  def build_changeset
+    {
+      vessel_info: build_vessel,
+      declarations: [applicant_email],
+      owners: build_owners,
+      documents_received: documents_received,
+      receipt_no: receipt_no,
+      payment_type: payment_type,
+    }
+  end
+
+  def build_vessel
+    {
+      name: vessel_name.blank? ? :unknown : vessel_name,
+      reg_no: vessel_reg_no,
+    }
+  end
+
+  def build_owners
+    [
+      {
+        name: applicant_name,
+        email: applicant_email,
+      },
+    ]
+  end
 end
