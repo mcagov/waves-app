3e76db2377ba772ed8e46c0a596d65de43f3c6be
Toby Privett <toby.privett@oceanshq.com>
2016-10-19 15:52:34 +0100

    Ensure payment input fields are upper case

--- app/assets/javascripts/ui-helpers.js
+++ app/assets/javascripts/ui-helpers.js
@@ -2,4 +2,11 @@ $(document).ready(function() {
   $('select.select2').select2({
     minimumResultsForSearch: Infinity
   });
+
+  // Upper case input fields
+  $(function() {
+    $('.upcase.form-control').focusout(function() {
+        this.value = this.value.toLocaleUpperCase();
+    });
+  });
 });
