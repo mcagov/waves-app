9537c78024fb182c3c6aee6c9250ade8e21dcffb
Toby Privett <toby.privett@oceanshq.com>
2016-10-19 14:48:16 +0100

    Define a policy: officer_intervention_required?

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -29,6 +29,10 @@ class Submission < ApplicationRecord
     Policies::Submission.approvable?(self)
   end
 
+  def officer_intervention_required?
+    Policies::Submission.officer_intervention_required?(self)
+  end
+
   def process_application(registration_starts_at)
     Builders::NewRegistrationBuilder.create(self, registration_starts_at)
   end
