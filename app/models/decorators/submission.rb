6c39cedbf0966bf7e36d218329f16a8cb6b7508e
Toby Privett <toby.privett@oceanshq.com>
2016-10-19 13:50:21 +0100

    Ensure that the task list can always be rendered
    - We need to handle varying degrees of data
    quality from the FinancePayment input

--- app/models/decorators/submission.rb
+++ app/models/decorators/submission.rb
@@ -19,4 +19,28 @@ class Decorators::Submission < SimpleDelegator
   def printed?(print_job_type)
     print_jobs && print_jobs[print_job_type.to_s].present?
   end
+
+  def vessel_name
+    return vessel.name if vessel.name
+    if finance_payment && finance_payment.vessel_name.present?
+      finance_payment.vessel_name
+    else
+      "Unkown"
+    end
+  end
+
+  def applicant_name
+    return correspondent if correspondent
+    if finance_payment && finance_payment.applicant_name.present?
+      finance_payment.applicant_name
+    else
+      "Unkown"
+    end
+  end
+
+  private
+
+  def finance_payment
+    payment.remittance if source.to_sym == :manual_entry
+  end
 end
