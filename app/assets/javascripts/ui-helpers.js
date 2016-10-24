2ba016aa7df152545fcb3af8a58111ab5e52b13e
Toby Privett <toby.privett@oceanshq.com>
2016-10-24 09:57:57 +0100

    Ensure that our upcase function persists

--- app/assets/javascripts/ui-helpers.js
+++ app/assets/javascripts/ui-helpers.js
@@ -2,11 +2,8 @@ $(document).ready(function() {
   $('select.select2').select2({
     minimumResultsForSearch: Infinity
   });
-
-  // Upper case input fields
-  $(function() {
-    $('.upcase.form-control').focusout(function() {
-        this.value = this.value.toLocaleUpperCase();
-    });
-  });
 });
+
+$(document).on('keyup','.upcase.form-control', {} ,function(e){
+  $(this).val($(this).val().toUpperCase());
+})
