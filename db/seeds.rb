4fc3e2ab94489e388ac32008adfdd9cc4130514a
Toby Privett <toby.privett@oceanshq.com>
2016-10-26 16:40:16 +0100

    Add user name to the db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ USERS = %w(alice bob charlie develop).freeze
 USERS.each do |user|
   u = User.find_or_initialize_by(name: user.humanize)
   u.email = "#{user}@example.com"
+  u.name = "#{user.titleize} Waves"
   u.password = "password"
   u.save!
 end
