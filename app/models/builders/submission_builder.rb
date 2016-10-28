cddeab644fcb33940b8590d082ebe1a70e322726
Toby Privett <toby.privett@oceanshq.com>
2016-10-28 15:09:50 +0100

    Implement a new Submission Builder
    Objective:
    - Build an interface doesn’t care about the state of the
    submission, i.e. a method that can call be 
    safely called at any point in the submission lifecycle
    
    Activities:
    - Build the registry_info dataset
    - Build the changeset from the registry_info
    - Build the declarations from the changeset

--- app/models/builders/submission_builder.rb
+++ app/models/builders/submission_builder.rb
@@ -1,16 +1,12 @@
 class Builders::SubmissionBuilder
   class << self
-    def create(submission)
+    def build_defaults(submission)
       @submission = submission
-
       ensure_defaults
-      build_declarations
 
-      @submission
-    end
-
-    def update(submission)
-      @submission = submission
+      build_registry_info if @submission.registered_vessel
+      build_changeset if @submission.registered_vessel
+      build_declarations if @submission.changeset
 
       @submission
     end
@@ -21,15 +17,31 @@ class Builders::SubmissionBuilder
       @submission.part ||= :part_3
       @submission.task ||= :new_registration
       @submission.source ||= :online
-      @submission.ref_no = RefNo.generate
+      @submission.ref_no ||= RefNo.generate
+    end
+
+    def build_registry_info
+      return unless @submission.registry_info.blank?
+
+      @submission.registry_info = {
+        vessel_info: @submission.registered_vessel.attributes,
+        owners: @submission.registered_vessel.owners.map(&:attributes),
+      }
+    end
+
+    def build_changeset
+      return unless @submission.changeset.blank?
+      @submission.changeset = @submission.registry_info
     end
 
     def build_declarations
+      return unless Declaration.where(submission: @submission).empty?
+
+      submitted_owners = @submission.symbolized_changeset[:owners]
+      completed_declarations = @submission.symbolized_changeset[:declarations]
+
       Builders::DeclarationBuilder.create(
-        @submission,
-        @submission.symbolized_changeset[:owners],
-        @submission.symbolized_changeset[:declarations]
-      )
+        @submission, submitted_owners, completed_declarations)
     end
   end
 end
--- spec/models/builders/submission_builder_spec.rb
+++ spec/models/builders/submission_builder_spec.rb
@@ -1,34 +1,141 @@
 require "rails_helper"
 
 describe Builders::SubmissionBuilder do
-  let!(:submission) { create(:submission) }
-
-  context "#create" do
-    subject { described_class.create(submission) }
-
-    it "defaults to task = new_registration" do
-      expect(submission.task.to_sym).to eq(:new_registration)
+  describe "#build_defaults" do
+    let!(:submission) do
+      Submission.new(
+        changeset: changeset,
+        registry_info: registry_info,
+        registered_vessel: registered_vessel,
+        declarations: declarations)
     end
 
-    it "defaults to source = online" do
-      expect(submission.source.to_sym).to eq(:online)
+    let!(:changeset) { nil }
+    let!(:registry_info) { nil }
+    let!(:registered_vessel) { nil }
+    let!(:declarations) { [] }
+
+    before { described_class.build_defaults(submission) }
+
+    context "in general" do
+      it "defaults to task = new_registration" do
+        expect(submission.task.to_sym).to eq(:new_registration)
+      end
+
+      it "defaults to source = online" do
+        expect(submission.source.to_sym).to eq(:online)
+      end
+
+      it "defaults to part = part_3" do
+        expect(submission.part.to_sym).to eq(:part_3)
+      end
+
+      it "builds the ref_no" do
+        expect(submission.ref_no).to be_present
+      end
+
+      it "does not build the registry info" do
+        expect(submission.registry_info).to be_nil
+      end
+
+      it "does not alter the changeset" do
+        expect(submission.changeset).to eq(changeset)
+      end
+
+      it "does not build any declarations" do
+        expect(submission.declarations).to be_empty
+      end
     end
 
-    it "defaults to part = part_3" do
-      expect(submission.part.to_sym).to eq(:part_3)
+    context "when the changeset is populated" do
+      let!(:changeset) { vessel_owner_sample_data }
+
+      it "builds a declaration for each of the two owners" do
+        expect(submission.declarations.count).to eq(2)
+      end
+
+      it "does not alter the changeset" do
+        expect(submission.symbolized_changeset[:vessel_info][:name])
+          .to eq("MY BOAT")
+        expect(submission.symbolized_changeset[:owners][0][:name])
+          .to eq("ALICE")
+      end
     end
 
-    it "builds the ref_no" do
-      expect(submission.ref_no).to be_present
-    end
+    context "with a registered vessel" do
+      let!(:registered_vessel) do
+        Register::Vessel.create(vessel_sample_data)
+      end
 
-    it "builds a declaration for each owner" do
-      expect(submission.declarations.count).to eq(submission.owners.count)
+      context "when the registry_info has not been set" do
+        it "builds the registry_info#vessel_info" do
+          expect(submission.symbolized_registry_info[:vessel_info][:name])
+            .to eq("MY BOAT")
+        end
+
+        it "builds the registry_info#owners" do
+          expect(submission.symbolized_registry_info[:owners][0][:name])
+            .to eq("ALICE")
+        end
+      end
+
+      context "when the registry_info is already populated" do
+        let!(:registry_info) { { foo: "bar" } }
+
+        it "does not alter the registry_info" do
+          expect(submission.symbolized_registry_info).to eq(foo: "bar")
+        end
+      end
+
+      context "when the changeset is blank" do
+        it "builds the changeset from the registry_info" do
+          expect(submission.symbolized_changeset)
+            .to eq(submission.symbolized_registry_info)
+        end
+      end
+
+      context "when the changeset is already populated" do
+        let!(:changeset) { { foo: "bar" } }
+
+        it "does not alter the changeset" do
+          expect(submission.symbolized_changeset[:foo]).to eq("bar")
+        end
+      end
+
+      context "when there are no declarations" do
+        it "builds the declarations from the changeset" do
+          expect(submission.reload.declarations[0].owner.name).to eq("ALICE")
+          expect(submission.declarations[1].owner.name).to eq("BOB")
+        end
+
+        context "running #build_defaults again" do
+          it "does not alter the declarations" do
+            described_class.build_defaults(submission)
+            expect(submission.reload.declarations.length).to eq(2)
+          end
+        end
+      end
     end
   end
+end
 
-  describe "#update" do
-    it "builds the registry_infor"
-    it "builds the registry_infor"
-  end
+def vessel_owner_sample_data
+  {
+    owners: owner_sample_data,
+    vessel_info: vessel_sample_data,
+  }
+end
+
+def vessel_sample_data
+  {
+    name: "MY BOAT", number_of_hulls: 1,
+    owners: owner_sample_data.map { |owner| Register::Owner.new(owner) }
+  }
+end
+
+def owner_sample_data
+  [
+    { name: "ALICE", email: "alice@example.com" },
+    { name: "BOB", email: "bob@example.com" },
+  ]
 end
