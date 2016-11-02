d082dd709e74970b89b428d7f3051183daa811e9
Toby Privett <toby@snaplab.co.uk>
2016-11-02 10:39:32 +0000

    Disable approval button for “other tasks”

--- app/models/policies/submission.rb
+++ app/models/policies/submission.rb
@@ -13,6 +13,11 @@ class Policies::Submission
 
     def approvable?(submission)
       @submission = submission
+
+      # disable approval for anything that is not a new_registration
+      # we will remove this when we build out the other task approvals
+      return false unless @submission.task.to_sym == :new_registration
+
       return false unless @submission.declarations.incomplete.empty?
       return false unless AccountLedger.paid?(@submission)
       true
