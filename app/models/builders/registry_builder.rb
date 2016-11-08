edb031fbc9ffd98b1c5091e7313fdffa6848b250
Toby Privett <toby@snaplab.co.uk>
2016-11-08 11:54:45 +0000

    Build Registry details for a change_details task

--- app/models/builders/registry_builder.rb
+++ app/models/builders/registry_builder.rb
@@ -4,46 +4,51 @@ class Builders::RegistryBuilder
       @submission = submission
 
       Register::Vessel.transaction do
-        create_vessel
-        assign_vessel
-        create_owners
+        init_vessel
+        update_vessel_details
+        assign_vessel_to_submission
+        build_owners
       end
     end
 
     private
 
-    # rubocop:disable Metrics/AbcSize
-    def create_vessel
-      # find_or_initialize
-      @vessel = Register::Vessel.create(
-        part: @submission.part,
-        name: @submission.vessel.name,
-        hin: @submission.vessel.hin,
-        make_and_model: @submission.vessel.make_and_model,
-        length_in_meters: @submission.vessel.length_in_meters,
-        number_of_hulls: @submission.vessel.number_of_hulls,
-        mmsi_number: @submission.vessel.mmsi_number,
-        radio_call_sign: @submission.vessel.radio_call_sign,
-        vessel_type: @submission.vessel.type_of_vessel)
+    def init_vessel
+      @vessel = @submission.registered_vessel
+      @vessel ||= Register::Vessel.new(part: @submission.part)
     end
 
-    def assign_vessel
-      @submission.update_attribute(:registered_vessel_id, @vessel.id)
+    def update_vessel_details
+      @vessel.name = @submission.vessel.name
+      @vessel.hin = @submission.vessel.hin
+      @vessel.make_and_model = @submission.vessel.make_and_model
+      @vessel.length_in_meters = @submission.vessel.length_in_meters
+      @vessel.number_of_hulls = @submission.vessel.number_of_hulls
+      @vessel.mmsi_number = @submission.vessel.mmsi_number
+      @vessel.radio_call_sign = @submission.vessel.radio_call_sign
+      @vessel.vessel_type = @submission.vessel.type_of_vessel
+      @vessel.save
     end
 
-    def create_owners
-      # @vessel.owners#delete_all ?
-
-      @submission.owners.each do |owner|
-        Register::Owner.create(
-          vessel: @vessel,
-          name: owner.name, nationality: owner.nationality,
-          email: owner.email, phone_number: owner.phone_number,
-          address_1: owner.address_1, address_2: owner.address_2,
-          town: owner.town, postcode: owner.postcode,
-          country: owner.country
-        )
+    def assign_vessel_to_submission
+      unless @submission.registered_vessel
+        @submission.update_attribute(:registered_vessel_id, @vessel.id)
       end
+    end
+
+    def build_owners
+      @vessel.owners.delete_all
+      @submission.owners.each { |owner| build_owner(owner) }
+    end
+
+    def build_owner(owner)
+      Register::Owner.create(
+        vessel: @vessel,
+        name: owner.name, nationality: owner.nationality,
+        email: owner.email, phone_number: owner.phone_number,
+        address_1: owner.address_1, address_2: owner.address_2,
+        town: owner.town, postcode: owner.postcode,
+        country: owner.country)
     end
   end
 end
--- spec/models/builders/registry_builder_spec.rb
+++ spec/models/builders/registry_builder_spec.rb
@@ -7,15 +7,15 @@ describe Builders::RegistryBuilder do
     let!(:submission) do
       create(:assigned_submission,
              changeset: {
-               vessel_info: build(:submission_vessel, vessel_type: "BARGE"),
+               vessel_info: build(:submission_vessel, name: "BOB BARGE"),
                owners: [{ name: "ALICE" }, { name: "BOB" }],
              })
     end
 
     let(:registered_vessel) { submission.reload.registered_vessel }
 
-    it "creates the expected vessel type" do
-      expect(registered_vessel.vessel_type).to eq("BARGE")
+    it "creates the expected vessel name" do
+      expect(registered_vessel.name).to eq("BOB BARGE")
     end
 
     it "sets the registry part" do
@@ -28,6 +28,33 @@ describe Builders::RegistryBuilder do
       expect(registered_vessel.owners.last.name).to eq("BOB")
     end
 
-    it "can find or create vessel and owners"
+    context "with a task that changes registry details" do
+      let!(:change_details_submission) do
+        create(:assigned_submission,
+               task: :change_vessel,
+               registered_vessel: registered_vessel,
+               changeset: {
+                 vessel_info: build(:submission_vessel, name: "DON DINGHY"),
+                 owners: [
+                   { name: "ALICE" }, { name: "DAVE" }, { name: "ELLEN" }],
+               })
+      end
+
+      before do
+        described_class.create(change_details_submission)
+        registered_vessel.reload
+      end
+
+      it "updates the vessel name" do
+        expect(registered_vessel.name).to eq("DON DINGHY")
+      end
+
+      it "creates registered owners in the expect order" do
+        expect(registered_vessel.owners.length).to eq(3)
+        expect(registered_vessel.owners[0].name).to eq("ALICE")
+        expect(registered_vessel.owners[1].name).to eq("DAVE")
+        expect(registered_vessel.owners[2].name).to eq("ELLEN")
+      end
+    end
   end
 end
