724a4f6707322572ce60412b1044733797cbc945
Toby Privett <toby@snaplab.co.uk>
2016-11-08 13:19:55 +0000

    Define business rules for application processing

--- app/models/submission/application_processor.rb
+++ app/models/submission/application_processor.rb
@@ -2,9 +2,14 @@ class Submission::ApplicationProcessor
   class << self
     def run(submission, registration_starts_at)
       @submission = submission
+      @task = Task.new(@submission.task)
 
-      Builders::RegistryBuilder.create(@submission)
-      Builders::RegistrationBuilder.create(@submission, registration_starts_at)
+      Builders::RegistryBuilder.create(@submission) if @task.builds_registry?
+
+      if @task.prints_certificate?
+        Builders::RegistrationBuilder
+          .create(@submission, registration_starts_at)
+      end
     end
   end
 end
--- spec/models/submission/application_processor_spec.rb
+++ spec/models/submission/application_processor_spec.rb
@@ -2,13 +2,13 @@ require "rails_helper"
 
 describe Submission::ApplicationProcessor do
   context "#run" do
-    let(:submission) { create(:assigned_submission, task: task) }
     let(:registration_starts_at) { 1.week.ago }
 
     subject { described_class.run(submission, registration_starts_at) }
 
     context "new_registration" do
       let(:task) { :new_registration }
+      let(:submission) { create(:submission) }
 
       before do
         expect_registry_builder
@@ -16,6 +16,90 @@ describe Submission::ApplicationProcessor do
       end
 
       it { subject }
+    end
+
+    context "with a registered_vessel" do
+      let(:submission) do
+        create(:assigned_submission,
+               task: task, registered_vessel: create(:registered_vessel))
+      end
+
+      context "change_registry_details" do
+        let(:task) { :change_registry_details }
+
+        before do
+          expect_registry_builder
+          expect_registration_builder
+        end
+
+        it { subject }
+      end
+
+      context "change_address" do
+        let(:task) { :change_address }
+
+        before do
+          expect_registry_builder
+          dont_expect_registration_builder
+        end
+
+        it { subject }
+      end
+
+      context "duplicate_certificate" do
+        let(:task) { :duplicate_certificate }
+
+        before do
+          dont_expect_registry_builder
+          expect_registration_builder
+        end
+
+        it { subject }
+      end
+
+      context "renewal" do
+        let(:task) { :renewal }
+
+        before do
+          expect_registry_builder
+          expect_registration_builder
+        end
+
+        it { subject }
+      end
+
+      context "closure" do
+        let(:task) { :closure }
+
+        before do
+          dont_expect_registry_builder
+          dont_expect_registration_builder
+        end
+
+        it { subject }
+      end
+
+      context "transcript" do
+        let(:task) { :closure }
+
+        before do
+          dont_expect_registry_builder
+          dont_expect_registration_builder
+        end
+
+        it { subject }
+      end
+
+      context "reserve_name" do
+        let(:task) { :reserve_name }
+
+        before do
+          dont_expect_registry_builder
+          dont_expect_registration_builder
+        end
+
+        it { subject }
+      end
     end
   end
 end
@@ -26,8 +110,16 @@ def expect_registry_builder
     .with(submission)
 end
 
+def dont_expect_registry_builder
+  expect(Builders::RegistryBuilder).not_to receive(:create)
+end
+
 def expect_registration_builder
   expect(Builders::RegistrationBuilder)
     .to receive(:create)
     .with(submission, registration_starts_at)
+end
+
+def dont_expect_registration_builder
+  expect(Builders::RegistrationBuilder).not_to receive(:create)
 end
