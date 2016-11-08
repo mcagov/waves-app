3c896ec2eb17b2cc56faa3ca7f4a7577a52c3355
Toby Privett <toby@snaplab.co.uk>
2016-11-08 12:17:44 +0000

    Tidy up the application processor spec

--- spec/models/submission/application_processor_spec.rb
+++ spec/models/submission/application_processor_spec.rb
@@ -2,25 +2,32 @@ require "rails_helper"
 
 describe Submission::ApplicationProcessor do
   context "#run" do
+    let(:submission) { create(:assigned_submission, task: task) }
+    let(:registration_starts_at) { 1.week.ago }
+
     subject { described_class.run(submission, registration_starts_at) }
 
-    context "a new_registration" do
-      let(:submission) { create(:assigned_submission) }
-      let(:registration_starts_at) { 1.week.ago }
+    context "new_registration" do
+      let(:task) { :new_registration }
 
       before do
-        expect(Builders::RegistryBuilder)
-          .to receive(:create)
-          .with(submission)
-
-        expect(Builders::RegistrationBuilder)
-          .to receive(:create)
-          .with(submission, registration_starts_at)
+        expect_registry_builder
+        expect_registration_builder
       end
 
       it { subject }
     end
-
-    it "checks behaviour of other tasks"
   end
+end
+
+def expect_registry_builder
+  expect(Builders::RegistryBuilder)
+    .to receive(:create)
+    .with(submission)
+end
+
+def expect_registration_builder
+  expect(Builders::RegistrationBuilder)
+    .to receive(:create)
+    .with(submission, registration_starts_at)
 end
