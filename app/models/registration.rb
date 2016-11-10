c786d5c18622db4137dc95ccac8b9a37b74dba03
Toby Privett <toby@snaplab.co.uk>
2016-11-10 11:27:02 +0000

    Enable Registrations without Submissions
    - When we come to loading historic data, we will
    need to build Registrations that weren’t produced
    by Submissions

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,6 +1,6 @@
 class Registration < ApplicationRecord
-  belongs_to :submission
-  belongs_to :actioned_by, class_name: "User"
+  belongs_to :submission, required: false
+  belongs_to :actioned_by, class_name: "User", required: false
   belongs_to :vessel,
              class_name: "Register::Vessel", foreign_key: "vessel_id"
 end
