8a5540d1d38c45b0be7dfd9ed173c1b71a0e20c9
Toby Privett <toby@snaplab.co.uk>
2016-11-11 10:27:20 +0000

    Enable user#to_s to display user name in views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :trackable, :validatable
+
+  def to_s
+    name
+  end
 end
