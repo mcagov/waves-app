30dd5ea260ca8fb55e9824ed02759ae45052acff
Toby Privett <toby@snaplab.co.uk>
2016-11-16 13:41:52 +0000

    Create notifications even if they can’t be sent
    - This might be a questionable strategy
    
    If there is no email address, we are still creating
    the notification object. Within the #send method
    there is a guard that checks whether the notification
    is deliverable?
    
    The idea is that, while the notification won’t be
    sent, there will be an audit trail that someone
    tried to send the email. We can then inspect the notification
    record and see whether it could have been delivered.

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,11 +1,31 @@
 require "rails_helper"
 
 describe Notification, type: :model do
-  context "#create" do
-    before { create(:notification) }
+  describe "#create" do
+    context "with valid params" do
+      let!(:notification) { create(:notification) }
 
-    it "delivers the email" do
-      expect(Delayed::Job.count).to eq(1)
+      it "is deliverable?" do
+        expect(notification).to be_deliverable
+      end
+
+      it "delivers the email" do
+        expect(Delayed::Job.count).to eq(1)
+      end
+    end
+
+    context "with no recipient_email and recipient_name" do
+      let!(:notification) do
+        Notification.create(recipient_name: nil, recipient_email: nil)
+      end
+
+      it "is not deliverable?" do
+        expect(notification).not_to be_deliverable
+      end
+
+      it "does not attempt to deliver the email" do
+        expect(Delayed::Job.count).to eq(0)
+      end
     end
   end
 end
