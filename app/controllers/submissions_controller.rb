116b3cfffcbeeffda8cf689680bfee8bdd7a4011
Toby Privett <toby@snaplab.co.uk>
2016-11-22 12:08:42 +0000

    Save a new submission

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -8,6 +8,16 @@ class SubmissionsController < InternalPagesController
     @submission = Submission.new
   end
 
+  def create
+    @submission = Submission.new(submission_params)
+
+    if @submission.save
+      redirect_to submission_path(@submission)
+    else
+      render :new
+    end
+  end
+
   def show
     @submission = Decorators::Submission.new(@submission)
   end
@@ -40,6 +50,7 @@ class SubmissionsController < InternalPagesController
   # rubocop:disable Metrics/MethodLength
   def submission_params
     params.require(:submission).permit(
+      :applicant_name, :applicant_email,
       vessel: [
         :name, :hin, :make_and_model, :length_in_meters, :number_of_hulls,
         :vessel_type, :vessel_type_other, :mmsi_number, :radio_call_sign],
--- spec/features/submissions/user_adds_new_submission_spec.rb
+++ spec/features/submissions/user_adds_new_submission_spec.rb
@@ -9,10 +9,12 @@ feature "User adds a new submission", type: :feature do
     select("New Registration", from: "Application Type")
     fill_in("Date Document Received", with: "1/1/2016")
 
-    # fill_in("Applicant Name", with: "Bob")
-    # fill_in("Applicant's Email Address", with: "bob@example.com")
-    # check("Send an application receipt email")
+    fill_in("Applicant Name", with: "Bob")
+    fill_in("Applicant's Email Address", with: "bob@example.com")
+    check("Send an Application Receipt email")
 
-    # click_on("Save Application")
+    click_on("Save Application")
+
+    expect(page).to have_css("h1", text: "New Registration ID: ")
   end
 end
