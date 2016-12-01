67cdf85e254c6345cabe8a65e07b68574b1bde09
Toby Privett <toby@snaplab.co.uk>
2016-12-01 15:21:27 +0000

    Validate the Work Log fields

--- app/models/work_log.rb
+++ app/models/work_log.rb
@@ -2,6 +2,12 @@ class WorkLog < ApplicationRecord
   belongs_to :submission
   belongs_to :actioned_by, class_name: "User"
 
+  scope :in_part, ->(part) { where(part: part.to_sym) }
+
+  validates :submission_id, presence: true
+  validates :actioned_by_id, presence: true
+  validates :part, presence: true
+
   class << self
     def activities_for(_loggable)
       []
