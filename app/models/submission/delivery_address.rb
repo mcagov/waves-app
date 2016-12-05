ca8349598e9a9a944a920a2ddba98e818804b818
Toby Privett <toby@snaplab.co.uk>
2016-12-05 16:29:31 +0000

    Remove models that are now in WavesUtilities

--- app/models/submission/delivery_address.rb
+++ app/models/submission/delivery_address.rb
@@ -1,8 +1,2 @@
-class Submission::DeliveryAddress
-  include VirtualAddress
-  include VirtualModel
-
-  def inline_name_and_address
-    "#{name}, #{inline_address}" if name && !inline_address.empty?
-  end
+class Submission::DeliveryAddress < WavesUtilities::DeliveryAddress
 end
