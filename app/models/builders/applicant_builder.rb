28c946bedb2f1d0c0c7c6d6e6b0474805af9da17
Toby Privett <toby@snaplab.co.uk>
2016-12-07 15:10:48 +0000

    Build the applicant name / email from the changeset

---
+++ app/models/builders/applicant_builder.rb
@@ -0,0 +1,40 @@
+class Builders::ApplicantBuilder
+  class << self
+    def build(submission)
+      @submission = submission
+      @changeset = @submission.symbolized_changeset
+      @correspondent = load_correspondent if @changeset
+
+      if @correspondent
+        @submission.applicant_name = @correspondent[:name]
+        @submission.applicant_email = @correspondent[:email]
+      end
+
+      @submission
+    end
+
+    private
+
+    def load_correspondent
+      correspondent_key == :agent ? load_agent : load_owner
+    end
+
+    def correspondent_key
+      (@changeset[:correspondent] || :owners_1).to_sym
+    end
+
+    def load_agent
+      @changeset[:agent]
+    end
+
+    def load_owners
+      @changeset[:owners] || []
+    end
+
+    def load_owner
+      load_owners.select do |owner|
+        owner[:id].to_sym == correspondent_key
+      end.first
+    end
+  end
+end
