06bfc546f75da4c4e347bbf0ffa0849e9c163c40
Toby Privett <toby@snaplab.co.uk>
2016-12-12 14:45:58 +0000

    No need to pass vessel_reg_no with access_code
    - We can authenticate with session_key and 
    access_code.

--- app/controllers/api/v1/vessels_controller.rb
+++ app/controllers/api/v1/vessels_controller.rb
@@ -13,17 +13,14 @@ module Api
 
       private
 
-      def vessel_params
-        params[:id].split(";")
-      end
-
       def load_vessel
-        client_session = ClientSession.find_by(
-          vessel_reg_no: vessel_params[0],
-          access_code: vessel_params[1],
-          external_session_key: vessel_params[2])
+        vessel_params = params[:id].split(";")
 
-        client_session.vessel if client_session
+        client_session = ClientSession.find_by(
+          access_code: vessel_params[0],
+          external_session_key: vessel_params[1])
+
+        client_session.registered_vessel if client_session
       end
     end
   end
--- spec/controllers/api/v1/vessels_controller_spec.rb
+++ spec/controllers/api/v1/vessels_controller_spec.rb
@@ -5,41 +5,41 @@ describe Api::V1::VesselsController, type: :controller do
     let(:parsed_attrs) { JSON.parse(response.body)["data"]["attributes"] }
 
     let!(:vessel) { create(:registered_vessel) }
-    let!(:client_session) { create(:client_session, vessel: vessel) }
-    let(:external_session_key) { client_session.external_session_key }
+    let(:email) { vessel.customers.first.email }
 
-    let(:id) { "#{vessel_reg_no};#{access_code};#{external_session_key}" }
+    let!(:client_session) do
+      create(:client_session,
+             vessel_reg_no: vessel.reg_no,
+             email: email)
+    end
+
+    let(:access_code) { client_session.access_code }
+    let(:external_session_key) { client_session.external_session_key }
+    let(:id) { "#{access_code};#{external_session_key}" }
 
     before do
       get :show, params: { id: id }
     end
 
-    context "with a valid vessel reg_no" do
-      let(:vessel_reg_no) { vessel.reg_no }
-
-      context "and a valid access_code" do
-        let(:access_code) { client_session.access_code }
-
-        it "sets the content type" do
-          expect(response.content_type).to eq("application/json")
-        end
-
-        it "returns the registry_info" do
-          expect(parsed_attrs["registry_info"]["vessel_info"]["name"])
-            .to eq(vessel.name)
-        end
+    context "with a valid access_code" do
+      it "sets the content type" do
+        expect(response.content_type).to eq("application/json")
       end
 
-      context "and an invalid access_code" do
-        let(:access_code) { "000000" }
-
-        it { expect(response).to have_http_status(404) }
+      it "returns the registry_info" do
+        expect(parsed_attrs["registry_info"]["vessel_info"]["name"])
+          .to eq(vessel.name)
       end
     end
 
-    context "with an invalid vessel reg_no" do
-      let(:vessel_reg_no) { "foo" }
-      let(:access_code) { client_session.access_code }
+    context "and an invalid access_code" do
+      let(:access_code) { "000000" }
+
+      it { expect(response).to have_http_status(404) }
+    end
+
+    context "with an invalid session_key" do
+      let(:external_session_key) { "foo" }
 
       it { expect(response).to have_http_status(404) }
     end
