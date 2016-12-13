98b2cd2153655c42cd88430a3df4d45b389e40f2
Toby Privett <toby@snaplab.co.uk>
2016-12-13 17:16:15 +0000

    Edit applicant in the Vessel tab

--- spec/features/submissions/user_edits_submission_spec.rb
+++ spec/features/submissions/user_edits_submission_spec.rb
@@ -6,32 +6,16 @@ describe "User edits a submission", js: true do
     click_on "Edit Application"
   end
 
-  scenario "vessel: editing" do
+  scenario "vessel and applicant details" do
     fill_in("Vessel Name", with: "BOAT")
-    click_on("Save Application")
-
-    expect(page).to have_css(".vessel-name", text: "BOAT")
-  end
-
-  scenario "owners: removing Alice, then Adding Bob " do
-    click_on("Owners")
-    click_on("Remove Owner")
-    click_on("Add Individual Owner")
-
-    fill_in("Owner Name", with: "Bob")
-    click_on("Save Application")
-
-    click_on("Owners")
-    expect(page).to have_css("#declaration_1 .owner-name", text: "BOB")
-  end
-
-  scenario "applicant: editing" do
-    click_on("Applicant")
     fill_in("Applicant Name", with: "ANNIE")
     fill_in("Applicant's Email", with: "annie@example.com")
     click_on("Save Application")
 
+    expect(page).to have_css(".vessel-name", text: "BOAT")
     expect(page).to have_css(".applicant-name", text: "ANNIE")
     expect(page).to have_css(".applicant-email", text: "annie@example.com")
   end
+
+  scenario "owners"
 end
