5cd7035ee36df67ccffc73a47c542b0fae1286e4
Toby Privett <toby@snaplab.co.uk>
2016-12-15 10:30:07 +0000

    Move declarations controller to submission scope

---
+++ app/controllers/submission/declarations_controller.rb
@@ -0,0 +1,21 @@
+class Submission::DeclarationsController < InternalPagesController
+  before_action :load_submission
+
+  def complete
+    load_declaration
+    @declaration.completed_by = current_user
+    @declaration.declared! if @declaration.save
+
+    redirect_to submission_path(@declaration.submission)
+  end
+
+  protected
+
+  def load_submission
+    @submission = Submission.find(params[:submission_id])
+  end
+
+  def load_declaration
+    @declaration = @submission.declarations.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,6 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :declarations, only: [:create, :update]
-
   namespace :finance do
     resources :payments, only: [:new, :create, :show, :index]
   end
@@ -33,6 +31,13 @@ Rails.application.routes.draw do
 
   resources :submissions, only: [:new, :create, :show, :edit, :update] do
     resource :approval, controller: "submission/approvals", only: [:create]
+    resources :declarations,
+              controller: "submission/declarations",
+              only: [:create, :update] do
+      member do
+        post :complete
+      end
+    end
     resource :states, controller: "submission/states", only: [:show] do
       member do
         post :claim
