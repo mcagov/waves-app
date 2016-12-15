bee494c2a28a5256bd99333563729915085cab17
Toby Privett <toby@snaplab.co.uk>
2016-12-15 09:44:48 +0000

    Render the edit owner modal

--- spec/features/submissions/user_edits_submission_owners_spec.rb
+++ spec/features/submissions/user_edits_submission_owners_spec.rb
@@ -4,6 +4,7 @@ describe "User edits submission owners", js: true do
   before do
     visit_assigned_submission
     click_on "Edit Application"
+    click_on("Owners")
   end
 
   scenario "adding an owner" do
@@ -11,9 +12,15 @@ describe "User edits submission owners", js: true do
     click_on("Add Individual Owner")
 
     fill_in("Owner Name", with: "ALICE")
-    choose("FRANCE", from: "Nationality")
+    select("FRANCE", from: "Nationality")
     fill_in("Email", with: "alice@example.com")
     fill_in("Phone Number", with: "012345678")
+  end
 
+  scenario "editing an owner" do
+    owner_name = Declaration.last.owner.name
+    click_on(owner_name)
+    expect(find_field("Owner Name").value).to eq(owner_name)
+    fill_in("Owner Name", with: "ALICE")
   end
 end
