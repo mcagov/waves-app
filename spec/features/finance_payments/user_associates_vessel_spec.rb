b3738d662b144651357766cd85d04ea12fbced8b
Toby Privett <toby@snaplab.co.uk>
2016-12-30 13:01:01 +0000

    Display N/A as Official No. for a new registration

---
+++ spec/features/finance_payments/user_associates_vessel_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe "User associates vessel to finance_payment",
+         type: :feature, js: true do
+
+  scenario "hiding the Official No. for a change_vessel submission" do
+    create(:finance_payment, part: :part_3, task: :new_registration)
+
+    claim_submission_and_visit
+
+    within("#finance_info") do
+      expect(page).to have_css(".official_no", text: "N/A")
+    end
+  end
+end
