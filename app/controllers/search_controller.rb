0ecd120e548b2aaee5376e061366f77947ff6d15
Toby Privett <toby@snaplab.co.uk>
2016-12-30 14:33:05 +0000

    Render js from search based on param response_path
    - Here we decouple the search from the finance 
    payment modal

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,8 +5,9 @@ class SearchController < InternalPagesController
 
   def submissions
     @submissions = Search.submissions(params[:q])
+
     respond_to do |format|
-      format.js
+      format.js { render params[:response_path] }
     end
   end
 end
