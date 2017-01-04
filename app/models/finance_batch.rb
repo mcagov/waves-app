d5c19876b03345815da6abcf1c52e8c517689807
Toby Privett <toby@snaplab.co.uk>
2017-01-04 14:27:30 +0000

    Batches have a #total_amount

--- app/models/finance_batch.rb
+++ app/models/finance_batch.rb
@@ -2,8 +2,13 @@ class FinanceBatch < ApplicationRecord
   belongs_to :processed_by, class_name: "User"
   has_many :finance_payments, foreign_key: :batch_id,
                               class_name: "Payment::FinancePayment"
+  has_many :payments, through: :finance_payments
 
   protokoll :batch_no, pattern: "1#####"
+
+  def total_amount
+    payments.sum(&:amount)
+  end
 
   def toggle_state!
     closed? ? reopen_batch! : close_batch!
--- spec/models/finance_batch_spec.rb
+++ spec/models/finance_batch_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 describe FinanceBatch do
-  context ".toggle_state!" do
-    let(:batch) { create(:finance_batch) }
+  let!(:batch) { create(:finance_batch) }
 
+  context ".toggle_state!" do
     it "is not closed" do
       expect(batch).not_to be_closed
     end
@@ -17,5 +17,16 @@ describe FinanceBatch do
         it { expect(batch).not_to be_closed }
       end
     end
+  end
+
+  context ".total_amount" do
+    before do
+      expect(batch)
+        .to receive(:payments).and_return(
+          [create(:payment, amount: 2020), create(:payment, amount: 1111)]
+        )
+    end
+
+    it { expect(batch.total_amount).to eq(3131) }
   end
 end
