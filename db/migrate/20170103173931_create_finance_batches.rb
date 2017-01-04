e7383cdcd34cd0169668abe538e781682a688c32
Toby Privett <toby@snaplab.co.uk>
2017-01-04 10:56:12 +0000

    Reconsider finance batch fields
    - Original design was driven by the UI fields, but
    this gave us attributes like #ends_at and methods
    like #end!
    
    - Prefer #closed and #close

--- db/migrate/20170103173931_create_finance_batches.rb
+++ db/migrate/20170103173931_create_finance_batches.rb
@@ -2,10 +2,9 @@ class CreateFinanceBatches < ActiveRecord::Migration[5.0]
   def change
     create_table :finance_batches, id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
       t.uuid :finance_payment_id, index: true
-      t.datetime :starts_at
-      t.uuid :started_by_id
-      t.datetime :ends_at
-      t.uuid :ended_by_id
+      t.datetime :opened_at
+      t.datetime :closed_at
+      t.uuid :processed_by_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -101,10 +101,9 @@ ActiveRecord::Schema.define(version: 20170104093613) do
 
   create_table "finance_batches", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
     t.uuid     "finance_payment_id"
-    t.datetime "starts_at"
-    t.uuid     "started_by_id"
-    t.datetime "ends_at"
-    t.uuid     "ended_by_id"
+    t.datetime "opened_at"
+    t.datetime "closed_at"
+    t.uuid     "processed_by_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.integer  "batch_no"
