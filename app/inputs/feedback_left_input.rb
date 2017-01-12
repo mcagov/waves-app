a15a63cca27f1b3b5e142c93d6f226781b7a0279
Toby Privett <toby@snaplab.co.uk>
2017-01-12 09:16:30 +0000

    Prefer %span over %i for feedback tags

--- app/inputs/feedback_left_input.rb
+++ app/inputs/feedback_left_input.rb
@@ -2,6 +2,6 @@ class FeedbackLeftInput < SimpleForm::Inputs::Base
   def input(_wrapper_options)
     @builder.text_field(
       attribute_name, class: "form-control has-feedback-left") +
-      template.content_tag(:i, "", class: "form-control-feedback left")
+      template.content_tag(:span, "", class: "form-control-feedback left")
   end
 end
--- app/inputs/feedback_right_input.rb
+++ app/inputs/feedback_right_input.rb
@@ -2,6 +2,6 @@ class FeedbackRightInput < SimpleForm::Inputs::Base
   def input(_wrapper_options)
     @builder.text_field(
       attribute_name, class: "form-control has-feedback-right") +
-      template.content_tag(:i, "", class: "form-control-feedback right")
+      template.content_tag(:span, "", class: "form-control-feedback right")
   end
 end
