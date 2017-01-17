c47c9bb0771a42682b770331135079b2d321beb5
Toby Privett <toby@snaplab.co.uk>
2017-01-17 13:54:21 +0000

    Setup templates for extended owner fields

--- spec/features/part_2/user_saves_owner_details_spec.rb
+++ spec/features/part_2/user_saves_owner_details_spec.rb
@@ -4,5 +4,7 @@ describe "User save owner details", js: :true do
   scenario "in general" do
     visit_name_approved_part_2_submission
     click_on("Owners & Shareholding")
+
+    click_on("Add Individual Owner")
   end
 end
