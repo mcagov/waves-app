262bbe70e39b3fb9f63e2c1d1104d9604b3c30a6
Toby Privett <toby@snaplab.co.uk>
2017-01-17 17:00:30 +0000

    WIP - User adds a Corporate Owner

--- spec/features/part_2/user_saves_owner_details_spec.rb
+++ spec/features/part_2/user_saves_owner_details_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe "User save owner details", js: :true do
-  scenario "in general" do
+  scenario "Individual owner" do
     visit_name_approved_part_2_submission
     click_on("Owners & Shareholding")
     click_on("Add Individual Owner")
@@ -17,5 +17,18 @@ describe "User save owner details", js: :true do
 
     expect(Submission.last.declarations.last.owner.eligibility_status.to_sym)
       .to eq(:status_b)
+  end
+
+  xscenario "Corporate owner" do
+    visit_name_approved_part_2_submission
+    click_on("Owners & Shareholding")
+    click_on("Add Corporate Owner")
+
+    fill_in("Name", with: "Bob Inc")
+    fill_in("Registration Number", with: "1234567")
+    fill_in("Date of Incorporation ", with: "12/01/2017")
+    click_on("Save Individual Owner")
+
+    expect(page).to have_css(".owner-registration_number", text: "1234567")
   end
 end
