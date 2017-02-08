e1162c66ca4bbdaf210a223555b584849d2e3436
Toby Privett <toby@snaplab.co.uk>
2017-02-08 16:49:40 +0000

    Enable a Total Share count (stubbed to zero)

---
+++ app/models/submission/share_holding.rb
@@ -0,0 +1,8 @@
+class Submission::ShareHolding
+  def initialize(_submission)
+  end
+
+  def total
+    0
+  end
+end
---
+++ spec/features/part_2/user_edits_outright_shares_held_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+describe "User edits outright shares held", js: :true do
+  scenario "with one owner" do
+    visit_name_approved_part_2_submission
+
+    expect(page).to have_css("#total_shares", text: "Total shares allocated: 0")
+  end
+end
