1243dc69fb0ea46af71474b125896b91fa2c6e78
Toby Privett <toby@snaplab.co.uk>
2017-02-09 13:50:42 +0000

    Render joint shareholders list after group is created

--- spec/features/part_2/user_edits_shares_held_jointly_spec.rb
+++ spec/features/part_2/user_edits_shares_held_jointly_spec.rb
@@ -1,14 +1,17 @@
 require "rails_helper"
 
 describe "User edits shares held jointly", js: :true do
-  scenario "with one owner" do
+  scenario "adding a group with one owner" do
     visit_name_approved_part_2_submission
-
     owner_name = Declaration.last.owner.name
+
     click_on("Add Group of Joint Shareholders")
     select(owner_name, from: "Owner Name")
     click_on("Add Owner to New Group")
 
-    expect(Declaration::Group.count).to eq(1)
+    within("#shares_held_jointly") do
+      expect(page).to have_css(".owner_name", text: owner_name)
+      expect(page).to have_css(".shares_held", text: "0")
+    end
   end
 end
