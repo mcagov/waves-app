888ec9c8a4607ad0d03700549eea486214eaeb0f
Toby Privett <toby@snaplab.co.uk>
2017-02-09 11:23:54 +0000

    Rename spec

--- spec/features/part_2/user_edits_shares_held_outright_spec.rb
+++ spec/features/part_2/user_edits_shares_held_outright_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe "User edits outright shares held", js: :true do
+describe "User edits shares held outright", js: :true do
   scenario "with one owner" do
     visit_name_approved_part_2_submission
     expect(page).to have_css("#total_shares", text: "Total shares allocated: 0")
