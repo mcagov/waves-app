0c0ac2933c6423f8695415c824f68d744f7f30b7
Toby Privett <toby@snaplab.co.uk>
2017-02-10 12:16:22 +0000

    Scope .edit_in_place to shareholding

--- app/assets/javascripts/shareholding.js
+++ app/assets/javascripts/shareholding.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('a.edit_in_place').editable({
+  $('a.edit_shareholding_in_place').editable({
     success:
       function(e, editable) {
         var submission_id = $('#submission-id').html();
