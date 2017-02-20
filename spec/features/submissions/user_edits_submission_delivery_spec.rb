715a927cc3fb1b287610bca245165443b6ef863a
Toby Privett <toby@snaplab.co.uk>
2017-02-20 10:20:40 +0000

    Rename spec as we will be editing delivery address

--- spec/features/submissions/user_edits_submission_delivery_spec.rb
+++ spec/features/submissions/user_edits_submission_delivery_spec.rb
@@ -1,9 +1,11 @@
 require "rails_helper"
 
-feature "User views Delivery Address details", type: :feature, js: true do
-  scenario "in general" do
+feature "User edits Delivery Address details", type: :feature, js: true do
+  before do
     visit_assigned_submission
+  end
 
+  scenario "viewing and editing" do
     click_on("Payment")
     within("#delivery_address") do
       expect(page).to have_text("BOB DOLE, 11 DOWNING ST, WHITEHALL")
