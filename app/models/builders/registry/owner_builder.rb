d749ff987fb1a40ea1cd5536334d88d1bf17b2b6
Toby Privett <toby@snaplab.co.uk>
2017-02-22 11:17:47 +0000

    Introduce Builders::Registry::OwnerBuilder

---
+++ app/models/builders/registry/owner_builder.rb
@@ -0,0 +1,47 @@
+class Builders::Registry::OwnerBuilder
+  class << self
+    def create(submission, vessel)
+      @submission = submission
+      @vessel = vessel
+
+      perform
+
+      @vessel
+    end
+
+    private
+
+    def perform
+      @vessel.owners.delete_all
+      @submission.declarations.each { |declaration| build_owner(declaration) }
+    end
+
+    # rubocop:disable all
+    def build_owner(declaration)
+      owner = declaration.owner
+      registered_owner = @vessel.owners.create(
+        name: owner.name,
+        nationality: owner.nationality,
+        email: owner.email,
+        phone_number: owner.phone_number,
+        address_1: owner.address_1,
+        address_2: owner.address_2,
+        address_3: owner.address_3,
+        town: owner.town,
+        postcode: owner.postcode,
+        country: owner.country,
+        imo_number: owner.imo_number,
+        eligibility_status: owner.eligibility_status,
+        registration_number: owner.registration_number,
+        date_of_incorporation: owner.date_of_incorporation,
+        managing_owner: @submission.managing_owner_id == declaration.id,
+        correspondent: @submission.correspondent_id == declaration.id,
+        entity_type: declaration.entity_type,
+        shares_held: declaration.shares_held)
+
+      declaration.update_attribute(
+        :registered_owner_id, registered_owner.id)
+    end
+    # rubocop:enable all
+  end
+end
--- app/models/builders/registry_builder.rb
+++ app/models/builders/registry_builder.rb
@@ -83,43 +83,10 @@ class Builders::RegistryBuilder
     end
 
     def build_vessel_associations
-      build_owners
+      @vessel = Builders::Registry::OwnerBuilder.create(@submission, @vessel)
       @vessel = Builders::Registry::AgentBuilder.create(@submission, @vessel)
       @vessel = Builders::Registry::ShareBuilder.create(@submission, @vessel)
       @vessel = Builders::Registry::EngineBuilder.create(@submission, @vessel)
     end
-
-    def build_owners
-      @vessel.owners.delete_all
-      @submission.declarations.each { |declaration| build_owner(declaration) }
-    end
-
-    # rubocop:disable all
-    def build_owner(declaration)
-      owner = declaration.owner
-      registered_owner = @vessel.owners.create(
-        name: owner.name,
-        nationality: owner.nationality,
-        email: owner.email,
-        phone_number: owner.phone_number,
-        address_1: owner.address_1,
-        address_2: owner.address_2,
-        address_3: owner.address_3,
-        town: owner.town,
-        postcode: owner.postcode,
-        country: owner.country,
-        imo_number: owner.imo_number,
-        eligibility_status: owner.eligibility_status,
-        registration_number: owner.registration_number,
-        date_of_incorporation: owner.date_of_incorporation,
-        managing_owner: @submission.managing_owner_id == declaration.id,
-        correspondent: @submission.correspondent_id == declaration.id,
-        entity_type: declaration.entity_type,
-        shares_held: declaration.shares_held)
-
-      declaration.update_attribute(
-        :registered_owner_id, registered_owner.id)
-    end
-    # rubocop:enable all
   end
 end
