7130b212d45e8e6e98d7b092c8329ab6c6bf5674
Toby Privett <toby@snaplab.co.uk>
2017-02-23 10:44:56 +0000

    Define mortgage associations

--- app/models/mortgage.rb
+++ app/models/mortgage.rb
@@ -1,5 +1,10 @@
 class Mortgage < ApplicationRecord
   belongs_to :parent, polymorphic: true
+  has_many :mortgagees, inverse_of: :mortgage
+
+  # rubocop:disable Style/AlignHash
+  accepts_nested_attributes_for :mortgagees, allow_destroy: true,
+    reject_if: proc { |attributes| attributes["name"].blank? }
 
   class << self
     def types_for(submission)
--- app/models/mortgagee.rb
+++ app/models/mortgagee.rb
@@ -1,3 +1,3 @@
 class Mortgagee < ApplicationRecord
-  belongs_to :mortgage
+  belongs_to :mortgage, inverse_of: :mortgagees
 end
