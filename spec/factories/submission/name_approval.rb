0ee5e3f9080298ac07a9eadf4de5aa123ba26b6c
Toby Privett <toby@snaplab.co.uk>
2017-03-02 17:13:50 +0000

    Update Workflow Policy with check for name_approval

--- spec/factories/submission/name_approval.rb
+++ spec/factories/submission/name_approval.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
   factory :submission_name_approval, class: "Submission::NameApproval" do
-    name       Faker::Name
-    submission { create(:submission) }
+    part                :part_2
+    sequence(:name)     { |n| "Name #{n}" }
+    sequence(:port_no)  { |n| n }
+    port_code           "AA"
+    approved_until      { 90.days.from_now }
+    submission          { create(:submission, part: :part_2) }
   end
 end
