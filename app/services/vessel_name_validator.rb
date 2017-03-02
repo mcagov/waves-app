a67c216e8ddc32784ae19dc8626124f0bec3b955
Toby Privett <toby@snaplab.co.uk>
2017-03-02 15:50:46 +0000

    Implement the VesselNameValidator

--- app/services/vessel_name_validator.rb
+++ app/services/vessel_name_validator.rb
@@ -1,23 +1,33 @@
 class VesselNameValidator
-  def initialize(name_approval)
-    @name_approval = name_approval
-  end
+  class << self
+    def valid?(part, name, port_code)
+      @part = part
+      @name = name
+      @port_code = port_code
 
-  def name_in_use?
-    Register::Vessel
-      .in_part(@name_approval.part)
-      .where(name: @name_approval.name)
-      .where(port_code: @name_approval.port_code)
-      .where("name_approved_until is null or name_approved_until > now()")
-      .exists?
-  end
+      return false if vessel_exists?
+      return false if name_approval_exists?
 
-  def port_no_in_use?
-    Register::Vessel
-      .in_part(@name_approval.part)
-      .where(port_no: @name_approval.port_no)
-      .where(port_code: @name_approval.port_code)
-      .where("name_approved_until is null or name_approved_until > now()")
-      .exists?
+      true
+    end
+
+    private
+
+    def vessel_exists?
+      Register::Vessel
+        .in_part(@part)
+        .where(name: @name)
+        .where(port_code: @port_code)
+        .exists?
+    end
+
+    def name_approval_exists?
+      Submission::NameApproval
+        .in_part(@part)
+        .where(name: @name)
+        .where(port_code: @port_code)
+        .where("approved_until is null or approved_until > now()")
+        .exists?
+    end
   end
 end
