ca30993a32a5c694bb3e84cd610fff7f2da7aaef
Toby Privett <toby@snaplab.co.uk>
2017-03-06 16:44:23 +0000

    Officer submits the Carving & Marking form

---
+++ app/controllers/submission/carving_and_marking_controller.rb
@@ -0,0 +1,34 @@
+class Submission::CarvingAndMarkingController < InternalPagesController
+  before_action :load_submission
+
+  def create
+    @carving_and_marking = CarvingAndMarking.new(carving_and_marking_params)
+    @carving_and_marking.submission = @submission
+    @carving_and_marking.issued_by = current_user
+
+    @carving_and_marking.save
+
+    render_update_js
+  end
+
+  protected
+
+  def carving_and_marking_params
+    params.require(:carving_and_marking).permit(
+      :delivery_method, :tonnage_type)
+  end
+
+  def load_submission
+    @submission = Submission.find(params[:submission_id])
+  end
+
+  def render_update_js
+    respond_to do |format|
+      @modal_id = params[:modal_id]
+      @submission = Decorators::Submission.new(@submission)
+      format.js do
+        render "/submissions/extended/forms/carving_and_marking/update"
+      end
+    end
+  end
+end
--- spec/features/part_2/user_issues_carving_note_spec.rb
+++ spec/features/part_2/user_issues_carving_note_spec.rb
@@ -1,12 +1,22 @@
 require "rails_helper"
 
 describe "User issues a Carving & Marking Note", js: true do
-  scenario "in general" do
+  scenario "by email" do
     visit_carving_and_marking_ready_submission
     click_on("Certificates & Documents")
 
-    within("#carving .status") do
+    within("#carving_and_marking .status") do
       click_on("Issue Carving & Marking Note")
+    end
+
+    within(".modal-content") do
+      select("Register Tonnage", from: "Tonnage Type")
+      select("Send via Email", from: "Delivery Method")
+      click_on("Issue Carving & Marking note")
+    end
+
+    within("#carving_and_marking") do
+      expect(page).to have_css(".delivery_method", "Email")
     end
   end
 
@@ -14,7 +24,7 @@ describe "User issues a Carving & Marking Note", js: true do
     visit_name_approved_part_2_submission
     click_on("Certificates & Documents")
 
-    within("#carving .status") do
+    within("#carving_and_marking .status") do
       expect(page).to have_css(".red", text: "Official Number required")
       expect(page).to have_css(".red", text: "Net or Register Tonnage required")
     end
