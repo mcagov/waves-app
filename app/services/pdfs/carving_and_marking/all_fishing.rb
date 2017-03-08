4238d2aacd578d81c6a2a1c8ef2c533397a33fe5
Toby Privett <toby@snaplab.co.uk>
2017-03-08 11:03:14 +0000

    Remove duplicate method

--- app/services/pdfs/carving_and_marking/all_fishing.rb
+++ app/services/pdfs/carving_and_marking/all_fishing.rb
@@ -23,41 +23,6 @@ class Pdfs::CarvingAndMarking::AllFishing < Pdfs::CarvingAndMarking::Base
     @pdf.bounding_box([lmargin, 510], width: 510) { @pdf.stroke_horizontal_rule }
   end
 
-  def draw_certify
-    set_copy_font
-    @pdf.draw_text "I certify that I have inspected the above-named vessel and it has been *carved and marked/marked",
-                  at: [lmargin, 370]
-    @pdf.draw_text "(*delete as appropriate) in accordance with the instructions above and overleaf.",
-                  at: [lmargin, 355]
-
-    @pdf.draw_text "Signature", at: [lmargin, 320]
-    @pdf.bounding_box([100, 315], width: 180) { @pdf.stroke_horizontal_rule }
-    @pdf.draw_text "Office Stamp or Inspector's address", at: [300, 320]
-
-    @pdf.draw_text "Name", at: [lmargin, 290]
-    @pdf.bounding_box([100, 285], width: 180) { @pdf.stroke_horizontal_rule }
-
-    @pdf.draw_text "Date", at: [lmargin, 260]
-    @pdf.bounding_box([100, 255], width: 180) { @pdf.stroke_horizontal_rule }
-
-    @pdf.draw_text "Telephone No", at: [300, 260]
-    @pdf.bounding_box([380, 255], width: 120) { @pdf.stroke_horizontal_rule }
-
-    @pdf.draw_text "When this form has been completed, the Inspector of Marks should send or hand this form to the",
-                   at: [lmargin, 220]
-    @pdf.draw_text "address below, the only exception is if he/she seals the certificate in an envelope, signs across the seal",
-                   at: [lmargin, 205]
-    @pdf.draw_text "and hands it to the owner or charterer. It can then be sent or handed in, but the seal must be intact.",
-                   at: [lmargin, 190]
-
-    @pdf.draw_text "This form must be completed and returned to:", at: [lmargin, 135]
-    @pdf.draw_text "UK Ship Register", at: [lmargin, 120]
-    @pdf.draw_text "Anchor Court", at: [lmargin, 105]
-    @pdf.draw_text "KEEN ROAD", at: [lmargin, 90]
-    @pdf.draw_text "Cardiff", at: [lmargin, 75]
-    @pdf.draw_text "CF24 5JW", at: [lmargin, 60]
-  end
-
   def draw_marking_notes
     set_copy_font
     @pdf.draw_text "The above vessel is to be marked with the following:", at: [lmargin, 490]
