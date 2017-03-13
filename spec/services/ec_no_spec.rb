72b0a2f6f5fd0d24cf61762262061cf857816d8b
Toby Privett <toby@snaplab.co.uk>
2017-03-13 09:13:29 +0000

    Simplify EC Number
    - We don’t need to assign and EC Number by
    submission. Instead, we can use the registered
    vessel - if there is one.

--- spec/services/ec_no_spec.rb
+++ spec/services/ec_no_spec.rb
@@ -1,34 +1,14 @@
 require "rails_helper"
 
 describe EcNo do
-  context ".for_submission" do
-    subject { described_class.for_submission(submission) }
-
-    context "is blank by default" do
-      let(:submission) { build(:submission) }
-
-      it { expect(subject).to be_blank }
-    end
-
-    context "with a pleasure vessel" do
-      let(:submission) { create(:pleasure_submission) }
-      let(:vessel) { submission.registered_vessel }
-
-      it { expect(subject).to be_blank }
-    end
-
-    context "with a fishing vessel" do
-      let(:submission) { create(:fishing_submission) }
-      let(:vessel) { submission.registered_vessel }
-
-      it "is formatted" do
-        expect(subject).to eq("GBR000#{vessel.reg_no}")
-      end
-    end
-  end
-
   context ".for_vessel" do
     subject { described_class.for_vessel(vessel) }
+
+    context "is blank by default" do
+      let(:vessel) { build(:registered_vessel) }
+
+      it { expect(subject).to be_blank }
+    end
 
     context "with a pleasure vessel" do
       let(:vessel) { create(:pleasure_vessel) }
