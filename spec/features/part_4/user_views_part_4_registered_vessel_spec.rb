b638b55f5e98550e9cfae1cdca5b08774c6c71ee
Toby Privett <toby@snaplab.co.uk>
2017-03-16 15:25:49 +0000

    Fix faulty specs

--- spec/features/part_4/user_views_part_4_registered_vessel_spec.rb
+++ spec/features/part_4/user_views_part_4_registered_vessel_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper"
 
 describe "User views Part 4 registered vessel", type: :feature, js: true do
-  scenario "UI elements" do
-    visit_name_approved_part_4_submission
-    expect_mortgages(false)
-    expect_port_no_fields(false)
-    expect_service_description_fields(false)
+  before do
+    @vessel = create(:registered_vessel, part: :part_4)
+    login_to_part_4
+    visit vessels_path
+    click_on(@vessel.name.upcase)
   end
 
-  scenario "Name Approval page" do
-    visit_assigned_part_4_submission
+  scenario "UI Elements" do
+    expect_mortgages(false)
     expect_port_no_fields(false)
   end
 end
--- spec/features/part_4/user_views_part_4_submission_spec.rb
+++ spec/features/part_4/user_views_part_4_submission_spec.rb
@@ -1,15 +1,17 @@
 require "rails_helper"
 
 describe "User views Part 4 submission", type: :feature, js: true do
-  before do
+  scenario "UI elements" do
     visit_name_approved_part_4_submission
-  end
-
-  scenario "UI Elements" do
     expect_mortgages(false)
     expect_port_no_fields(false)
     expect_service_description_fields(false)
     expect_last_registry_fields(false)
     expect_underlying_registry_fields(true)
+  end
+
+  scenario "Name Approval page" do
+    visit_assigned_part_4_submission
+    expect_port_no_fields(false)
   end
 end
