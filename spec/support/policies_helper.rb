12f5183dda34b1e171412c6c7af5acd0e55534ef
Toby Privett <toby@snaplab.co.uk>
2017-03-16 14:30:55 +0000

    Tidy up helper file

--- spec/support/policies_helper.rb
+++ spec/support/policies_helper.rb
@@ -1,26 +1,27 @@
-def expect_mortgages(bln)
-  css = "#mortgages-tab"
+def expect_field(bln, css)
   if bln
     expect(page).to have_css(css)
   else
     expect(page).not_to have_css(css)
   end
+end
+
+def expect_mortgages(bln)
+  expect_field(bln, "#mortgages-tab")
 end
 
 def expect_port_no_fields(bln)
-  css = ".port_no_fields"
-  if bln
-    expect(page).to have_css(css)
-  else
-    expect(page).not_to have_css(css)
-  end
+  expect_field(bln, ".port_no_fields")
+end
+
+def expect_service_description_fields(bln)
+  expect_field(bln, ".service_description_fields")
+end
+
+def expect_underlying_registry_fields(bln)
+  expect_field(bln, ".underlying_registry_fields")
 end
 
 def expect_referral_button(bln)
-  css = ".btn-refer-submission"
-  if bln
-    expect(page).to have_css(css)
-  else
-    expect(page).not_to have_css(css)
-  end
+  expect_field(bln, ".btn-refer-submission")
 end
