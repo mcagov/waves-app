668b6af0a3fed7f32ada6217d9726745ce21f7e1
Toby Privett <toby@snaplab.co.uk>
2017-03-17 13:08:59 +0000

    Manager has a SafetyManagement address

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,2 +1,8 @@
 class Manager < Customer
+  has_one :safety_management, as: :parent, dependent: :destroy
+
+  accepts_nested_attributes_for(
+    :safety_management,
+    allow_destroy: true,
+    reject_if: proc { |attributes| attributes["address_1"].blank? })
 end
