c6026d5ccbf8ecde3118a47f14fe230120d1d14f
Toby Privett <toby@snaplab.co.uk>
2017-03-20 16:28:36 +0000

    Implemement Part 1 & 4 vessel types

--- spec/features/part_1/user_edits_vessel_type_spec.rb
+++ spec/features/part_1/user_edits_vessel_type_spec.rb
@@ -4,10 +4,10 @@ describe "User edits vessel type", js: :true do
   scenario "in general" do
     visit_name_approved_part_1_submission
 
-    select("GENERAL CARGO SHIP", from: "Vessel Category")
-    expect(page).to have_css(".vessel_type_label", text: "Type of GENERAL")
+    select("DREDGER", from: "Vessel Category")
+    expect(page).to have_css(".vessel_type_label", text: "Type of DREDGER")
 
-    select("BARGE", from: "Vessel Category")
-    expect(page).to have_css(".vessel_type_label", text: "Type of BARGE")
+    select("OIL TANKER", from: "Vessel Category")
+    expect(page).to have_css(".vessel_type_label", text: "Type of OIL TANKER")
   end
 end
