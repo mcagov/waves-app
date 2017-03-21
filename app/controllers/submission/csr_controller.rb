1310bae334bd234deb7609978afdda5d1a63bcba
Toby Privett <toby@snaplab.co.uk>
2017-03-21 15:47:59 +0000

    Save the CSR form with the extended#saved modal

--- app/controllers/submission/csr_controller.rb
+++ app/controllers/submission/csr_controller.rb
@@ -5,10 +5,23 @@ class Submission::CsrController < InternalPagesController
     @csr_form = Builders::CsrFormBuilder.build(@submission)
   end
 
+  def update
+    @csr_form = @submission.csr_form
+    @csr_form.update_attributes(csr_form_params)
+
+    respond_to do |format|
+      format.js { render "/submissions/extended/forms/update" }
+    end
+  end
+
   protected
 
   def load_submission
     @submission =
       Submission.in_part(current_activity.part).find(params[:submission_id])
+  end
+
+  def csr_form_params
+    params.require(:csr_form).permit(CsrForm.attribute_names)
   end
 end
--- spec/features/part_1/user_issues_csr_spec.rb
+++ spec/features/part_1/user_issues_csr_spec.rb
@@ -1,9 +1,16 @@
 require "rails_helper"
 
-describe "User views issues CSR", type: :feature do
+describe "User views issues CSR", js: true do
   scenario "in general" do
     visit_assigned_csr_submission
 
-    expect(find("#csr_vessel_name").value).to eq("Registered Boat 1")
+    expect(find("#csr_form_vessel_name").value).to eq("Registered Boat 1")
+    find("#csr_form_remarks").set("NO COMMENT")
+    click_on("Save Details")
+
+    expect(page)
+      .to have_link("Go to Task List", href: tasks_my_tasks_path)
+
+    expect(CsrForm.last.remarks).to eq("NO COMMENT")
   end
 end
