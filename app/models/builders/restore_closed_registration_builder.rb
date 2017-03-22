33c1bc828f0a9ab73a30ea6420e97cdc8fcb4b53
Toby Privett <toby@snaplab.co.uk>
2017-03-22 11:15:58 +0000

    Update application processor and builders

--- app/models/builders/restore_closed_registration_builder.rb
+++ app/models/builders/restore_closed_registration_builder.rb
@@ -5,20 +5,22 @@ class Builders::RestoreClosedRegistrationBuilder
       @registered_vessel = @submission.registered_vessel
       @previous_registration = @registered_vessel.current_registration
 
-      create_closed_registration
+      create_restored_registration
     end
 
     private
 
-    def create_closed_registration
-      Registration.create(
+    def create_restored_registration
+      registration = Registration.create(
         vessel_id: @registered_vessel.id,
         registered_at: @previous_registration.try(:registered_at),
         registered_until: @previous_registration.try(:registered_until),
-        submission_ref_no: @submission.ref_no,
-        task: @submission.task,
         registry_info: @registered_vessel.registry_info,
         actioned_by: @submission.claimant)
+
+      @submission.update_attributes(registration: registration)
+
+      registration
     end
   end
 end
--- app/models/submission/application_processor.rb
+++ app/models/submission/application_processor.rb
@@ -5,30 +5,35 @@ class Submission::ApplicationProcessor
       @task = Task.new(@submission.task)
       @approval_params = approval_params
 
-      process_changes
+      assign_registered_vessel
+      assign_registration
+
       build_print_jobs
     end
 
     private
 
-    def process_changes
-      @registered_vessel = build_registry if @task.builds_registry?
-
-      @registration =
-        if @task.builds_registration?
-          build_registration
-        elsif @task == :closure
-          build_closed_registration
+    def assign_registered_vessel
+      @registered_vessel =
+        if @task.builds_registry?
+          Builders::RegistryBuilder.create(@submission)
         else
-          clone_current_registration
+          @submission.registered_vessel
         end
     end
 
-    def build_registry
-      Builders::RegistryBuilder.create(@submission)
+    def assign_registration
+      @registration =
+        if @task.builds_registration?
+          build_new_registration
+        elsif @task == :closure
+          build_closed_registration
+        else
+          build_cloned_registration
+        end
     end
 
-    def build_registration
+    def build_new_registration
       Builders::RegistrationBuilder
         .create(
           @submission,
@@ -43,7 +48,7 @@ class Submission::ApplicationProcessor
           @approval_params[:closure_reason])
     end
 
-    def clone_current_registration
+    def build_cloned_registration
       Builders::ClonedRegistrationBuilder.create(@submission)
     end
 
@@ -52,7 +57,8 @@ class Submission::ApplicationProcessor
       return if @submission.electronic_delivery?
 
       Builders::PrintJobBuilder
-        .create(@registration, @submission.part, @task.print_job_templates)
+        .create(
+          @submission.registration, @submission.part, @task.print_job_templates)
     end
   end
 end
