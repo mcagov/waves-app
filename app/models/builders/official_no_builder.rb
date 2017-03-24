3588ef6661a107ca05a90e5ec6c95ff522eb7145
Toby Privett <toby@snaplab.co.uk>
2017-03-24 15:45:20 +0000

    OfficialNoBuilder accepts param: vessel_reg_no

--- app/models/builders/official_no_builder.rb
+++ app/models/builders/official_no_builder.rb
@@ -1,11 +1,13 @@
 class Builders::OfficialNoBuilder
   class << self
-    def build(submission)
+    def build(submission, vessel_reg_no = nil)
       @submission = submission
 
       @submission.registered_vessel ||=
         Register::Vessel.create(
-          name: @submission.vessel.name, part: @submission.part)
+          name: @submission.vessel.name,
+          part: @submission.part,
+          reg_no: vessel_reg_no)
 
       @submission.save
     end
--- spec/models/builders/official_no_builder_spec.rb
+++ spec/models/builders/official_no_builder_spec.rb
@@ -4,8 +4,16 @@ describe Builders::OfficialNoBuilder do
   context ".build" do
     let(:submission) { create(:submission, part: :part_2) }
 
-    before { described_class.build(submission) }
+    context "a system-generated number" do
+      before { described_class.build(submission) }
 
-    it { expect(submission.vessel_reg_no).to be_present }
+      it { expect(submission.vessel_reg_no).to be_present }
+    end
+
+    context "a user-input number" do
+      before { described_class.build(submission, "VALID_NO") }
+
+      it { expect(submission.vessel_reg_no).to eq("VALID_NO") }
+    end
   end
 end
