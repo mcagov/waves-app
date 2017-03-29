6ed364c43fc37739d770ce1b5a6122fcca6a038a
Toby Privett <toby@snaplab.co.uk>
2017-03-29 10:38:27 +0100

    Replace trix <div> tags with <p> tags in email template
    - However, we render the default message text with <div>
    tags to support trix formatting

--- app/helpers/message_helper.rb
+++ app/helpers/message_helper.rb
@@ -11,28 +11,30 @@ module MessageHelper
   end
 
   def cancel_email_text(submission)
-    %(<p>Thank you for your application regarding the vessel
-      #{submission.vessel}</p>
-      <p>Please note that your application has been cancelled
-      for the following reason/s</p>
-      <p>[FREE TEXT]</p>
-      <p>Where applicable the fee will be refunded using the same
-      method and to the same payee as the original payment.</p>
-      <p>If you require any further assistance please do not
-      hesitate to contact us at #{contact_us_for_part}.</p>
-      <p>Please quote your application reference
-      #{submission.ref_no} in all correspondence.</p>)
+    %(<div>Thank you for your application regarding the vessel
+      #{submission.vessel}
+      <br><br>Please note that your application has been cancelled
+      for the following reason/s
+      <br><br>
+      [FREE TEXT]
+      <br><br>Where applicable the fee will be refunded using the same
+      method and to the same payee as the original payment.
+      <br><br>If you require any further assistance please do not
+      hesitate to contact us at #{contact_us_for_part}.
+      <br><br>Please quote your application reference
+      #{submission.ref_no} in all correspondence.</div>)
   end
 
   def refer_email_text(submission)
-    %(<p>Thank you for your application regarding the vessel
-      #{submission.vessel}</p>
-      <p>In order for us to proceed with your application we
-      will require the following:</p>
-      <p>[FREE TEXT]</p>
-      <p>If you require any further assistance please do not
-      hesitate to contact us at #{contact_us_for_part}.</p>
-      <p>Please quote your application reference
-      #{submission.ref_no} in all correspondence.</p>)
+    %(<div>Thank you for your application regarding the vessel
+      #{submission.vessel}
+      <br><br>In order for us to proceed with your application we
+      will require the following:
+      <br><br>
+      [FREE TEXT]
+      <br><br>If you require any further assistance please do not
+      hesitate to contact us at #{contact_us_for_part}.
+      <br><br>Please quote your application reference
+      #{submission.ref_no} in all correspondence.</div>)
   end
 end
