6fcf7ab0b5d5a6fccfa626b771a00c82457e1281
Toby Privett <toby@snaplab.co.uk>
2017-04-28 11:12:08 +0100

    Revert "Ensure Re-registration does not show registry_info"
    This reverts commit a32076d82d47dccad7d831eda89b21e44b96c843.
    
    - This approach was incorrect. For a re_registration want to show the changeset,
    not the registry_info.

--- app/services/task.rb
+++ app/services/task.rb
@@ -2,9 +2,4 @@ class Task < WavesUtilities::Task
   def new_registration?
     [:provisional, :new_registration].include?(@key)
   end
-
-  def display_changeset?
-    return false if @key == :re_registration
-    builds_registry?
-  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,10 +1,8 @@
 require "rails_helper"
 
 describe Task do
-  let(:task) { described_class.new(task_type) }
-
   context "#new_registration?" do
-    subject { task.new_registration? }
+    subject { described_class.new(task_type).new_registration? }
 
     context ":new_registration" do
       let(:task_type) { :new_registration }
@@ -22,36 +20,6 @@ describe Task do
       let(:task_type) { :renewal }
 
       it { expect(subject).to be_falsey }
-    end
-  end
-
-  context "#display_changeset?" do
-    subject { task.display_changeset? }
-
-    context ":re_registration" do
-      let(:task_type) { :re_registration }
-
-      it { expect(subject).to be_falsey }
-    end
-
-    context ":new_registration" do
-      let(:task_type) { :new_registration }
-
-      before do
-        expect(task).to receive(:builds_registry?)
-      end
-
-      it { subject }
-    end
-
-    context ":vessel_details" do
-      let(:task_type) { :vessel_details }
-
-      before do
-        expect(task).to receive(:builds_registry?)
-      end
-
-      it { subject }
     end
   end
 end
