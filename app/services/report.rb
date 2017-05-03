076408b6857f73ce23ea07e8c35d2bbb5a86cb26
Toby Privett <toby@snaplab.co.uk>
2017-05-03 16:43:43 +0100

    Let’s not use eval()

--- app/services/report.rb
+++ app/services/report.rb
@@ -1,7 +1,10 @@
 class Report
   class << self
     def build(template, filters)
-      eval("Report::#{template.to_s.camelize}.new(#{filters})")
+      case template.to_sym
+      when :staff_performance
+        Report::StaffPerformance.new(filters)
+      end
     end
   end
 end
