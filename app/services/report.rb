cf1548b3e90ba77514adb9cf46592d668cc028a1
Toby Privett <toby@snaplab.co.uk>
2017-05-03 14:28:55 +0100

    Tidy up the Report class’s file path

--- app/services/report.rb
+++ app/services/report.rb
@@ -1,9 +1,28 @@
 class Report
-  def initialize(template)
-    @template = template
+  class << self
+    def find(template)
+      case template.to_sym
+      when :staff_performance
+        StaffPerformance.new
+      end
+    end
   end
 
-  def title
-    "Staff Performance"
+  class StaffPerformance
+    def title
+      "Staff Performance"
+    end
+
+    def columns
+      [:task_type, :total_transactions, :top_performer]
+    end
+
+    def rows
+      Task.all_task_types.map do |task_type|
+        [
+          Task.new(task_type[1]).description, "A", "B"
+        ]
+      end
+    end
   end
 end
