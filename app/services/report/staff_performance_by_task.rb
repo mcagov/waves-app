01aaa18e4fc6f85f1693214b6748fdc06b481e78
Toby Privett <toby@snaplab.co.uk>
2017-05-04 15:24:47 +0100

    Define filter_fields for #staff_performance_by_task

--- app/services/report/staff_performance_by_task.rb
+++ app/services/report/staff_performance_by_task.rb
@@ -2,4 +2,8 @@ class Report::StaffPerformanceByTask < Report
   def title
     "Staff Performance by Task"
   end
+
+  def filter_fields
+    [:filter_task, :filter_part, :filter_date_range]
+  end
 end
---
+++ spec/services/report/staff_performance_by_task_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe Report::StaffPerformanceByTask do
+  context "in general" do
+    before do
+      create(:completed_submission)
+    end
+
+    let(:filters) { { task: :new_registration } }
+
+    subject { described_class.new(filters) }
+
+    it "has a title" do
+      expect(subject.title).to eq("Staff Performance by Task")
+    end
+
+    it "has some filter_fields" do
+      expect(subject.filter_fields)
+        .to eq([:filter_task, :filter_part, :filter_date_range])
+    end
+  end
+end
