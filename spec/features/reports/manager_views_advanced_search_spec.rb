b7f0e37d79ccdce9f80a08597bccc4c836ec314f
Toby Privett <toby@snaplab.co.uk>
2017-06-06 16:05:03 +0100

    Persist the filter_operators

--- spec/features/reports/manager_views_advanced_search_spec.rb
+++ spec/features/reports/manager_views_advanced_search_spec.rb
@@ -4,5 +4,12 @@ describe "Manager views advanced search", js: true do
   scenario "in general" do
     login_to_reports
     click_on("Advanced Search")
+
+    find_field("filter[vessel[name_operator]]").select("exclude")
+    find_field("filter[vessel][name]").set("Bob")
+    click_on("Apply Filter")
+
+    expect(find_field("filter[vessel[name_operator]]").value).to eq("excludes")
+    expect(find_field("filter[vessel][name]").value).to eq("Bob")
   end
 end
