34a0c03eaded21fd31046e19bbe67202a7c8b4d9
Toby Privett <toby@snaplab.co.uk>
2017-06-08 09:35:53 +0100

    Extract form elements to the Section class

--- app/services/report/advanced_search/section.rb
+++ app/services/report/advanced_search/section.rb
@@ -12,4 +12,28 @@ class Report::AdvancedSearch::Section
   def fieldset_id
     "filter_#{@key}"
   end
+
+  def form_group_id(search_attr)
+    "fields_#{@key}_#{search_attr.key}"
+  end
+
+  def field_label(search_attr)
+    "filter_#{@key}_#{search_attr.key}"
+  end
+
+  def operator_field(search_attr)
+    "#{@key}[#{search_attr.key}_operator]"
+  end
+
+  def text_input_field(search_attr)
+    "filter[#{@key}][#{search_attr.key}]"
+  end
+
+  def add_search_criteria_label
+    "add_criteria[toggle_#{@key}]"
+  end
+
+  def add_search_criteria_field
+    "toggle_#{@key}"
+  end
 end
