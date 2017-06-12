78fb2d6647d0cfdb983abc5f76380824fbf0e0fb
Toby Privett <toby@snaplab.co.uk>
2017-06-12 09:05:21 +0100

    Existing reports are ‘Management Reports’

---
+++ spec/features/submissions/user_views_work_logs_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+describe "User views work logs", type: :feature do
+  scenario "in general" do
+    3.times { create(:work_log) }
+    login_to_part_3
+
+    visit reports_work_logs_path
+    expect(page).to have_css("#work_logs tbody tr", count: 3)
+  end
+end
