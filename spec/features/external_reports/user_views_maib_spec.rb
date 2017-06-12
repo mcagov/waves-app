95bdb7e41566c14e67b121f091d8231ab30bbead
Toby Privett <toby@snaplab.co.uk>
2017-06-12 16:28:56 +0100

    User selects the MAIB report to download

--- spec/features/external_reports/user_views_maib_spec.rb
+++ spec/features/external_reports/user_views_maib_spec.rb
@@ -7,20 +7,23 @@ describe "User views MAIB reports", js: true do
 
     find("#filter_date_start").set("01/01/2017")
     find("#filter_date_end").set("31/03/2017")
-    click_on("Apply Filter")
-
-    @results = page.all("table#results tr")
   end
 
   scenario "Registration Closures" do
-    within(@results[1]) { click_on("Download") }
+    select("Fishing Vessel Closures", from: "Report")
+    click_on("Apply Filter")
+
+    within("#results") { click_on("Download") }
 
     expect(page.response_headers["Content-Type"]).to match("application/xls")
     expect(page.text).to match("Worksheet ss:Name=\"Fishing Vessel Closures\"")
   end
 
   scenario "Quarterly Report" do
-    within(@results[3]) { click_on("Download") }
+    select("Quarterly Report", from: "Report")
+    click_on("Apply Filter")
+
+    within("#results") { click_on("Download") }
 
     expect(page.response_headers["Content-Type"]).to match("application/xls")
     expect(page.text).to match("Worksheet ss:Name=\"Under 12m\"")
