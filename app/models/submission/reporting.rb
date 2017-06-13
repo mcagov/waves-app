995385053dcf8c62e9b276e0092f1cfbe9ae5180
Toby Privett <toby@snaplab.co.uk>
2017-06-13 11:56:02 +0100

    Submission has register_length scopes

--- app/models/submission/reporting.rb
+++ app/models/submission/reporting.rb
@@ -1,13 +1,35 @@
 module Submission::Reporting
   class << self
-    def included(base)
+    def included(base) # rubocop:disable Metrics/MethodLength
       base.scope :flag_in, -> { where(task: Task.flag_in) }
       base.scope :flag_out, -> { where(task:  Task.flag_out) }
+
       base.scope :merchant, -> {}
       base.scope :fishing, -> {}
-      base.scope :under_15m, -> {}
-      base.scope :between_15m_24m, -> {}
-      base.scope :over_24m, -> {}
+
+      base.scope :under_15m, (lambda do
+        where(
+          "cast
+            (coalesce(nullif(
+              (changeset#>>'{vessel_info, register_length}'), ''), '0')
+                as numeric) < 15.0")
+      end)
+
+      base.scope :between_15m_24m, (lambda do
+        where(
+          "cast
+            (coalesce(nullif(
+              (changeset#>>'{vessel_info, register_length}'), ''), '0')
+                as numeric) between 15.0 and 24.0")
+      end)
+
+      base.scope :over_24m, (lambda do
+        where(
+          "cast
+            (coalesce(nullif(
+              (changeset#>>'{vessel_info, register_length}'), ''), '0')
+                as numeric) > 24.0")
+      end)
     end
   end
 end
--- spec/models/submission/reporting_spec.rb
+++ spec/models/submission/reporting_spec.rb
@@ -21,4 +21,38 @@ describe Submission::Reporting do
       end
     end
   end
+
+  xdescribe "merchant/fishing" do
+    let!(:fishing) { create(:fishing_submission) }
+    let!(:merchant) { create(:fishing_submission) }
+  end
+
+  describe "by register length" do
+    let!(:under_15m) do
+      create(:submission,
+             changeset: { vessel_info: { register_length: 12.0 } })
+    end
+
+    let!(:between_15m_24m) do
+      create(:submission,
+             changeset: { vessel_info: { register_length: 16 } })
+    end
+
+    let!(:over_24m) do
+      create(:submission,
+             changeset: { vessel_info: { register_length: "44" } })
+    end
+
+    context ".under_15m" do
+      it { expect(Submission.under_15m).to eq([under_15m]) }
+    end
+
+    context ".between_15m_24m" do
+      it { expect(Submission.between_15m_24m).to eq([between_15m_24m]) }
+    end
+
+    context ".over_24m" do
+      it { expect(Submission.over_24m).to eq([over_24m]) }
+    end
+  end
 end
