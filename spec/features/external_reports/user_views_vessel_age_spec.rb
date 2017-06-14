4eabdcf6a77f0a8ce43bbee0f5576361d5969a79
Toby Privett <toby@snaplab.co.uk>
2017-06-14 09:17:49 +0100

    Render Vessel Age by vessel type

--- spec/features/external_reports/user_views_vessel_age_spec.rb
+++ spec/features/external_reports/user_views_vessel_age_spec.rb
@@ -2,7 +2,15 @@ require "rails_helper"
 
 describe "User views Vessel Age reports", js: true do
   before do
-    create(:registered_vessel, vessel_type: "BARGE")
+    create(
+      :registered_vessel,
+      keel_laying_date: 15.months.ago,
+      vessel_type: "BARGE",
+      gross_tonnage: 100,
+      imo_number: "IMO1",
+      reg_no: "SSR12345",
+      name: "BOB")
+
     login_to_reports
     visit admin_report_path(:fishing_regional)
     click_on("Vessel Age")
@@ -12,6 +20,26 @@ describe "User views Vessel Age reports", js: true do
     expect_filter_fields(true)
     expect_link_to_export_or_print(true)
 
-    within("#results") { expect(page).to have_button("BARGE") }
+    within("#results") do
+      col = find_all("tr td")
+      within(col[1]) { expect(page).to have_text("1") }
+      within(col[2]) { expect(page).to have_text("1.25") }
+      within(col[3]) { expect(page).to have_text("100.0") }
+      within(col[0]) { click_on("BARGE") }
+    end
+
+    expect_filter_fields(false)
+    expect_link_to_export_or_print(true)
+    expect(page).to have_css("h1", text: "Vessel Age: BARGE")
+
+    within("#results") do
+      col = find_all("tr td")
+      within(col[0]) { expect(page).to have_text("BOB") }
+      within(col[1]) { expect(page).to have_text("Part III") }
+      within(col[2]) { expect(page).to have_text("IMO1") }
+      within(col[3]) { expect(page).to have_text("SSR12345") }
+      within(col[4]) { expect(page).to have_text("1.25") }
+      within(col[5]) { expect(page).to have_text("100.0") }
+    end
   end
 end
