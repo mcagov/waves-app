cf27bbbf18a89af04ee75a289f980494016df478
Toby Privett <toby@snaplab.co.uk>
2017-06-15 10:22:49 +0100

    Implement tonnage statistics for part 1

--- app/services/report/tonnage_statistic.rb
+++ app/services/report/tonnage_statistic.rb
@@ -4,16 +4,24 @@ class Report::TonnageStatistic < Report
   end
 
   def headings
-    ["Fleet", "Gross Tonnage", "Number of Vessels", "Average Age of Vessels"]
+    ["Fleet", "Number of Vessels", "Gross Tonnage", "Average Age of Vessels"]
   end
 
   def results
     FLEETS.map do |fleet|
-      Result.new([fleet[0]])
+      result = load_result(fleet[1])
+      data_elements =
+        [
+          fleet[0],
+          result[0],
+          result[1].to_f.round(2),
+          result[2].to_f.round(2)]
+
+      Result.new(data_elements)
     end
   end
 
-  # rubocop:disable all
+  # rubocop:disable Metrics/LineLength
   FLEETS = [
     ["Part I Merchant vessels under 100gt (R)", :p1_merchant_under_100_gt_registered],
     ["Part I Merchant vessels under 100gt (F)", :p1_merchant_under_100_gt_frozen],
@@ -28,7 +36,7 @@ class Report::TonnageStatistic < Report
     ["Total Part 1 Merchant vessels 500gt and over", :p1_merchant_over_500_gt],
 
     ["Total Part 1 Merchant vessels", :p1_merchant],
-    ["Total Part 1 Pleasure vessels", :p1_merchant],
+    ["Total Part 1 Pleasure vessels", :p1_pleasure],
     ["Part I Grand Total (Merchant and Pleasure", :p1],
 
     ["Part II Fishing Vessels (<15m) (overall length)", :p2_under_15m],
@@ -44,6 +52,67 @@ class Report::TonnageStatistic < Report
     ["Part IV Bareboat Charter Fishing (F)", :p4_fishing_frozen],
     ["Total Part IV Bareboat Charter", :p4],
 
-    ["Total Number of Vessels on the Registry", :all]].freeze
+    ["All Vessels on the Registry (R)", :registered],
+    ["All Vessels on the Registry (F)", :frozne]].freeze
+  # rubocop:enable Metrics/LineLength
+
+  def load_result(key)
+    query = vessel_filter(Register::Vessel, key)
+    query = query.select(select_query)
+    query = registration_filter(query)
+    query.map { |r| [r.num_reg, r.age, r.gt] }.first
+  end
+
+  def select_query
+    [
+      "COUNT(*) as num_reg",
+      "AVG(COALESCE(now()::DATE - "\
+      "keel_laying_date::DATE, 0)::FLOAT / 365) AS age",
+      "SUM(gross_tonnage) AS gt",
+    ]
+  end
+
+  # rubocop:disable all
+  def vessel_filter(query, key)
+    query = query.in_part(:part_1) if key =~ /^p1.*/
+
+    query = query.where(merchant) if key =~ /.*merchant.*/
+    query = query.where(pleasure) if key =~ /.*pleasure.*/
+
+    query = query.where(under_100_gt) if key =~ /.*under_100_gt.*/
+    query = query.where(between_100_to_499_gt) if key =~ /.*100_to_499_gt.*/
+    query = query.where(over_500_gt) if key =~ /.*over_500_gt.*/
+
+    query = query.not_frozen if key =~ /.*registered/
+    query = query.frozen if key =~ /.*_frozen/
+
+    query
+  end
   # rubocop:enable all
+
+  def merchant
+    "registration_type = 'commercial'"
+  end
+
+  def pleasure
+    "registration_type = 'pleasure'"
+  end
+
+  def under_100_gt
+    "COALESCE(gross_tonnage, 0) < 100"
+  end
+
+  def between_100_to_499_gt
+    "COALESCE(gross_tonnage, 0) BETWEEN 100 AND 499.999"
+  end
+
+  def over_500_gt
+    "COALESCE(gross_tonnage, 0) >= 500"
+  end
+
+  def registration_filter(query)
+    query.where(
+      "vessels.id IN (SELECT DISTINCT vessel_id FROM registrations "\
+      "WHERE registered_until > now() AND closed_at IS NULL)")
+  end
 end
