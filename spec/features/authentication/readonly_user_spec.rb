37f6534191097238c921a981d0a5662ea36741a8
Toby Privett <toby@snaplab.co.uk>
2017-07-06 10:59:08 +0100

    Hide Registrar Tools from read_only user

--- spec/features/authentication/readonly_user_spec.rb
+++ spec/features/authentication/readonly_user_spec.rb
@@ -2,13 +2,17 @@ require "rails_helper"
 
 describe "Read only user" do
   before do
+    create(:registered_vessel, name: "READONLY BOAT")
     sign_in(create(:read_only_user))
     visit("/")
   end
 
   scenario "in general" do
-    click_on("Part 1: Pleasure")
+    click_on("Part 3:")
     expect(page).to have_current_path("/vessels")
     expect(page).not_to have_css(".side-menu")
+
+    click_on("READONLY BOAT")
+    expect(page).not_to have_link("Registrar Tools")
   end
 end
