a14ebc594bdf4c3b6a06b0640b947a7c61f61e8e
Toby Privett <toby@snaplab.co.uk>
2017-07-06 11:14:42 +0100

    Hide submission ‘Add Note’ from read_only user

--- spec/features/authentication/readonly_user_spec.rb
+++ spec/features/authentication/readonly_user_spec.rb
@@ -2,18 +2,28 @@ require "rails_helper"
 
 describe "Read only user" do
   before do
-    create(:registered_vessel, name: "READONLY BOAT")
+    @submission = create(:completed_submission)
+    @vessel = @submission.registered_vessel
     sign_in(create(:read_only_user))
     visit("/")
+
+    click_on("Part 3:")
   end
 
-  scenario "in general" do
-    click_on("Part 3:")
+  scenario "viewing vessels" do
     expect(page).to have_current_path("/vessels")
     expect(page).not_to have_css(".side-menu")
 
-    click_on("READONLY BOAT")
+    click_on(@vessel.name.upcase)
     expect(page).not_to have_link("Registrar Tools")
+    expect(page).not_to have_link("Add Note")
+  end
+
+  scenario "viewing submissions" do
+    visit submission_path(@submission)
+
+    expect(page).not_to have_css("#actions")
+    expect(page).not_to have_link("Add Correspondence")
     expect(page).not_to have_link("Add Note")
   end
 end
