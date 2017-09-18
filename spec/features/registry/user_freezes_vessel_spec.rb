c8e1701836584b52427108cd284f5ee0b49449b9
Toby Privett <toby@snaplab.co.uk>
2017-09-18 10:54:15 +0100

    Officer adds notes when freezing/unfreezing a vessel

--- spec/features/registry/user_freezes_vessel_spec.rb
+++ spec/features/registry/user_freezes_vessel_spec.rb
@@ -7,11 +7,31 @@ describe "User freezes (and unfreezes) a vessel", type: :feature, js: true do
     click_on("Registrar Tools")
     click_on("Freeze Record")
 
+    within("#freeze-record") do
+      fill_in("Reason", with: "Because Alice said....")
+      find(:css, '.submit_freeze_record').trigger('click')
+    end
+
     expect(page)
       .to have_css(".registration_status", text: "Frozen")
 
+    click_on("Notes")
+    within("#notes") do
+      expect(page).to have_text("Because Alice said....")
+    end
+
     click_on("Registrar Tools")
     click_on("Unfreeze Record")
+
+    within("#unfreeze-record") do
+      fill_in("Reason", with: "Because Bob said....")
+      find(:css, '.submit_unfreeze_record').trigger('click')
+    end
+
+    click_on("Notes")
+    within("#notes") do
+      expect(page).to have_text("Because Bob said....")
+    end
 
     expect(page)
       .to have_css(".registration_status", text: "Registered")
