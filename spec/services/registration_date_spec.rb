d63bc5f8f43640dd6c42f6ad753a7941372b2939
Toby Privett <toby@snaplab.co.uk>
2017-09-18 15:12:41 +0100

    Set registration#espires_at for change_vessel tasks
    - Here we use the vessel's current #registered_until
    
    - The rule being: changing vessel details doesn't buy another 5 years

--- spec/services/registration_date_spec.rb
+++ spec/services/registration_date_spec.rb
@@ -27,5 +27,18 @@ describe RegistrationDate do
         expect(subject.ends_at.to_date).to eq(Date.new(2013, 2, 22))
       end
     end
+
+    context "a change_vessel" do
+      let(:submission) { build(:assigned_change_vessel_submission) }
+
+      it "sets starts_at to today" do
+        expect(subject.starts_at.to_date).to eq(Date.new(2012, 11, 24))
+      end
+
+      it "sets the ends_at to the vessel's registered_until" do
+        expect(subject.ends_at)
+          .to eq(submission.registered_vessel.registered_until)
+      end
+    end
   end
 end
