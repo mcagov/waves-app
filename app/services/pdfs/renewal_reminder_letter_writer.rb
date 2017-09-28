5d667b4c7ad1160665960bfd2a9b2355b2789901
Toby Privett <toby@snaplab.co.uk>
2017-09-28 15:28:54 +0100

    Layout the Renewal Reminder for Part 1

--- app/services/pdfs/renewal_reminder_letter_writer.rb
+++ app/services/pdfs/renewal_reminder_letter_writer.rb
@@ -4,6 +4,7 @@ class Pdfs::RenewalReminderLetterWriter
   def initialize(registration, pdf)
     @registration = registration
     @vessel = @registration.registered_vessel
+    @applicant_name = @vessel.correspondent
     @delivery_name_and_address = @registration.delivery_name_and_address
 
     @pdf = pdf
@@ -11,21 +12,27 @@ class Pdfs::RenewalReminderLetterWriter
 
   def write
     @pdf.start_new_page
+    draw_bg(1)
     init_stationary(@registration.updated_at)
     vessel_name
-    message
+    @pdf.start_new_page
+    draw_bg(2)
+    @pdf.start_new_page
+    draw_bg(3)
+    @pdf.start_new_page
+    draw_bg(4)
     @pdf
   end
 
   protected
 
+  def draw_bg(p)
+    @pdf.image "#{Rails.root}/public/pdf_images/renewals/part_1_registration_renewal_letter-#{p}.png",
+      scale: 0.35, at: [-26,810]
+  end
+
   def vessel_name
     set_bold_font
     @pdf.draw_text @vessel[:name], at: [l_margin, 530]
-  end
-
-  def message
-    set_copy_font
-    @pdf.draw_text "Pending text", at: [l_margin, 510]
   end
 end
