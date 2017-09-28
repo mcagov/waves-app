674e51c6651600b2b35d817e6cd9e5f98ecb4136
Toby Privett <toby@snaplab.co.uk>
2017-09-28 15:55:48 +0100

    Tidy up the pdf writer

--- app/services/pdfs/renewal_reminder_letter_writer.rb
+++ app/services/pdfs/renewal_reminder_letter_writer.rb
@@ -11,35 +11,46 @@ class Pdfs::RenewalReminderLetterWriter
   end
 
   def write
-    @pdf.start_new_page
-    draw_bg(1)
-    init_stationary(@registration.updated_at)
-    vessel_name
-    registered_until
-    @pdf.start_new_page
-    draw_bg(2)
-    details_box
-    @pdf.start_new_page
-    draw_bg(3)
-    @pdf.start_new_page
-    draw_bg(4)
+    page_1
+    page_2
+    page_3
+    page_4
+
     @pdf
   end
 
   protected
 
-  def draw_bg(p)
-    @pdf.image "#{Rails.root}/public/pdf_images/renewals/part_1_registration_renewal_letter-#{p}.png",
-      scale: 0.35, at: [-26,810]
-  end
+  def page_1
+    @pdf.start_new_page
+    draw_bg(1)
+    init_stationary(@registration.updated_at)
 
-  def vessel_name
     set_bold_font
     @pdf.draw_text @vessel[:name], at: [l_margin, 530]
+    @pdf.draw_text @registration.registered_until, at: [330, 476]
   end
 
-  def registered_until
-    @pdf.draw_text @registration.registered_until, at: [330, 476]
+  def page_2
+    @pdf.start_new_page
+    draw_bg(2)
+    details_box
+  end
+
+  def page_3
+    @pdf.start_new_page
+    draw_bg(3)
+  end
+
+  def page_4
+    @pdf.start_new_page
+    draw_bg(4)
+  end
+
+  def draw_bg(p)
+    dir = "#{Rails.root}/public/pdf_images/renewals/"
+    @pdf.image "#{dir}part_1_registration_renewal_letter-#{p}.png",
+               scale: 0.35, at: [-26, 810]
   end
 
   def details_box
