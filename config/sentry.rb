8ac8eb39fb2da29fd3f94370182de9301fbd7882
Toby Privett <toby@snaplab.co.uk>
2017-10-06 10:40:40 +0100

    Fix rubocop issues

--- config/sentry.rb
+++ config/sentry.rb
@@ -1,5 +1,5 @@
 Raven.configure do |c|
   c.dsn = ENV.fetch("SENTRY_DSN")
-  c.environments = ['staging', 'production']
+  c.environments = %w(staging production)
   c.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
