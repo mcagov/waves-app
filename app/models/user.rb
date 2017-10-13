c10592da2cfae4ab2ec4d42421e8c4385fa18948
Toby Privett <toby@snaplab.co.uk>
2017-10-13 10:20:26 +0100

    Try devise-async for Devise emails
    - When we send emails inline on staging, we get 
    an error:  Errno::EHOSTUNREACH for a mysterious
    IP address
    
    - Google suggests that this may be an issue with 
    the way host names are configure in the app
    
    - Here we attempt to bypass any server config
    issues by passing the email delivery to delayed job

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :trackable, :validatable,
-         :recoverable, :rememberable
+         :recoverable, :rememberable, :async
 
   enum access_level:
     [:read_only, :operational_user, :team_leader, :system_manager]
