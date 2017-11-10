8ad826b3de2bcdb4f66ea8447368a9c2d646d76b
Toby Privett <toby@snaplab.co.uk>
2017-11-10 11:49:47 +0000

    Set up conditions for the termination notice
    - 30 day section notice must have been issued
    - vessel must be frozen

--- spec/features/registry/user_issues_termination_notice_spec.rb
+++ spec/features/registry/user_issues_termination_notice_spec.rb
@@ -1,11 +1,15 @@
 require "rails_helper"
 
 describe "User issues a 7 Day Notice", type: :feature, js: true do
-  scenario "in general" do
-    visit_registered_vessel
+  scenario "when the vessel is frozen and the 30 day notice has been issued" do
+    login_to_part_3
+    vessel = create(:registered_vessel)
+    vessel.current_registration.update_attribute(:section_notice_at, Time.now)
+    vessel.update_attribute(:frozen_at, Time.now)
+    visit vessel_path(vessel)
 
     click_on("Registrar Tools")
-    click_on("Registration Closure: 7 Day Notice of Termination")
+    click_on(notice_of_termination_link)
 
     expect(page).to have_text("Task Complete")
 
@@ -24,5 +28,13 @@ describe "User issues a 7 Day Notice", type: :feature, js: true do
     creates_a_work_log_entry("Submission", :termination_notice)
   end
 
-  scenario "when the 30 day section notice has not been generated"
+  scenario "when the 30 day section notice has not been generated" do
+    visit_registered_vessel
+    click_on("Registrar Tools")
+    expect(page).not_to have_link(notice_of_termination_link)
+  end
+end
+
+def notice_of_termination_link
+  "Registration Closure: 7 Day Notice of Termination"
 end
