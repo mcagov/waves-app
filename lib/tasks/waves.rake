cab10471ea4f3e707e4fc7d19da4bc37f36095b9
Toby Privett <toby@snaplab.co.uk>
2017-11-17 16:39:55 +0000

    Close vessels that are 7 days past Termination Notice

--- lib/tasks/waves.rake
+++ lib/tasks/waves.rake
@@ -22,4 +22,26 @@ namespace :waves do
             registered_vessel.registrations.first.try(:id))
     end
   end
+
+  task close_terminated_vessels: :environment do
+    Register::Vessel.termination_notice_issued.each do |vessel|
+      section_notice = vessel.section_notices.last
+
+      next unless section_notice
+      next if section_notice.updated_at > 7.days.ago
+
+      vessel.update_attribute(:frozen_at, nil)
+      vessel.restore_active_state!
+
+      submission =
+        Builders::CompletedSubmissionBuilder.create(
+          :termination_notice,
+          vessel.part,
+          vessel,
+          section_notice.actioned_by)
+
+      Builders::ClosedRegistrationBuilder
+        .create(submission, Time.now, Task.new(:termination_notice).description)
+    end
+  end
 end
--- spec/lib/tasks/waves_spec.rb
+++ spec/lib/tasks/waves_spec.rb
@@ -19,4 +19,47 @@ describe "Waves" do
       expect(Submission.unassigned.length).to eq(1)
     end
   end
+
+  context "close_terminated_vessels" do
+    let!(:vessel_1) { create(:registered_vessel) }
+    let!(:vessel_2) { create(:registered_vessel) }
+    let!(:vessel_3) { create(:registered_vessel) }
+
+    before do
+      init_closeable_vessel(vessel_2)
+      init_non_closeable_vessel(vessel_3)
+
+      Rake::Task["waves:close_terminated_vessels"].invoke
+    end
+
+    it "leaves vessel_1 as registered " do
+      expect(vessel_1.reload.registration_status).to eq(:registered)
+    end
+
+    it "closes the registration for vessel_2" do
+      expect(vessel_2.reload.registration_status).to eq(:closed)
+      expect(vessel_2.reload.frozen_at).to be_nil
+      expect(vessel_2.reload.current_state).to eq(:active)
+    end
+
+    it "leaves vessel_3 as termination_notice_issued" do
+      expect(vessel_3.reload.current_state).to eq(:termination_notice_issued)
+    end
+  end
+end
+
+def init_closeable_vessel(vessel)
+  vessel.update_column(:frozen_at, Time.now)
+  section_notice = Register::SectionNotice.create(noteable: vessel)
+  vessel.issue_section_notice!
+  vessel.issue_termination_notice!
+  section_notice.update_column(:updated_at, 8.days.ago)
+end
+
+def init_non_closeable_vessel(vessel)
+  vessel.update_column(:frozen_at, Time.now)
+  section_notice = Register::SectionNotice.create(noteable: vessel)
+  vessel.issue_section_notice!
+  vessel.issue_termination_notice!
+  section_notice.update_column(:updated_at, 6.days.ago)
 end
