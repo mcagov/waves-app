0ba1aa018ede9e3ebc377123a0a0b49494300e42
Toby Privett <toby@snaplab.co.uk>
2017-12-12 14:20:47 +0000

    Respond to Brakeman issues
    - Here we ignore a few issues:
    
    1. Dynamic sql in scope: registration.rb
    The field name to be searched is constructed
    by a method that builds the SQL finder for 
    a nested JSON object
    
    2. Mass attribute assignment
    We let the API endpoints receive *anything* contained
    within `data`. This lets us collect whatever
    data is required within the GOV.UK site and
    push it into the submission

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,12 @@
 class DashboardsController < InternalPagesController
   layout "application"
   def show
-    redirect_to activity_root(params[:activity]) if params[:activity]
+    redirect_to activity_root(activity) if activity
+  end
+
+  protected
+
+  def activity
+    params[:activity]
   end
 end
