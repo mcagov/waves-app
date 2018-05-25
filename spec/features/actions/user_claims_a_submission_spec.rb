91944ea6c36779248dca4c0a64bd5e3fc5a341d6
Toby Privett <toby@snaplab.co.uk>
2018-05-25 11:20:53 +0100

    Add a 'Claim' button to the submission page
    - RSS haven't asked for this but it feels right to be able
    to claim a submission when you are looking at it,
    eg after a search

--- spec/features/actions/user_claims_a_submission_spec.rb
+++ spec/features/actions/user_claims_a_submission_spec.rb
@@ -4,16 +4,24 @@ feature "User claims a submission", type: :feature, js: true do
   before do
     create(:unassigned_submission)
     login_to_part_3
+
+    click_link("Unclaimed Tasks")
   end
 
   scenario "claim and unclaim a submission" do
-    click_link("Unclaimed Tasks")
-
     within("tr.submission") { click_on("Claim") }
     expect(page).not_to have_css("tr.submission")
 
     click_link("My Tasks")
     within("tr.submission") { click_on("Unclaim") }
     expect(page).not_to have_css("tr.submission")
+  end
+
+  scenario "claim a submission from the submission page" do
+    within("tr.submission") { click_on("Boaty Mc") }
+
+    click_on("Claim")
+    expect(page)
+      .to have_css(".alert-info", text: "successfully claimed this application")
   end
 end
