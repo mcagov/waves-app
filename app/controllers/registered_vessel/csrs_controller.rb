f2c9c9d1c80358d653c5a2ab7f157f0ed2d4c63f
Toby Privett <toby@snaplab.co.uk>
2018-05-29 14:35:44 +0100

    Reg Officer edits CSR numbers

--- app/controllers/registered_vessel/csrs_controller.rb
+++ app/controllers/registered_vessel/csrs_controller.rb
@@ -1,8 +1,8 @@
 class RegisteredVessel::CsrsController < InternalPagesController
-  before_action :load_vessel, only: [:create]
+  before_action :load_vessel, only: [:create, :update]
+  before_action :load_csr_form, only: [:show, :update]
 
   def show
-    @csr_form = CsrForm.find(params[:id])
     respond_to do |format|
       format.pdf { build_and_render_pdf }
     end
@@ -15,7 +15,23 @@ class RegisteredVessel::CsrsController < InternalPagesController
     redirect_to submission_path(@submission)
   end
 
+  def update
+    @csr_form.update_attributes(csr_form_params)
+
+    respond_to do |format|
+      format.js { @modal_id = params[:modal_id] }
+    end
+  end
+
   protected
+
+  def load_csr_form
+    @csr_form = CsrForm.find(params[:id])
+  end
+
+  def csr_form_params
+    params.require(:csr_form).permit(:issue_number)
+  end
 
   def build_and_render_pdf
     if @csr_form
