64793f35980f8087db0f44978f901caf51147a25
Toby Privett <toby@snaplab.co.uk>
2018-05-30 15:23:14 +0100

    Enable #individual? at the base customer level

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -42,4 +42,8 @@ class Customer < ApplicationRecord
   def inline_name_and_address
     "#{name}, #{inline_address}" if name && !inline_address.empty?
   end
+
+  def individual?
+    (entity_type || "").to_sym != :corporate
+  end
 end
--- app/models/declaration/owner.rb
+++ app/models/declaration/owner.rb
@@ -1,5 +1,2 @@
 class Declaration::Owner < Customer
-  def individual?
-    (entity_type || "").to_sym != :corporate
-  end
 end
