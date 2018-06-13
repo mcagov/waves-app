4e43f0fe882bda920049174022fddf216ab7f881
Toby Privett <toby@snaplab.co.uk>
2018-06-13 14:52:36 +0100

    Clockwork can run ruby classes. Clockwork can't run rake tasks
    (on Cloudfoundry)

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,14 +1,36 @@
 require "clockwork"
-
-# Require the full rails environment if needed
 require "./config/boot"
 require "./config/environment"
 
 include Clockwork
 
-every 1.day, "daily_jobs", at: "04:30" do
-  rake "waves:unclaim_claimed_submissions"
-  rake "waves:expire_referrals"
-  rake "waves:process_reminders"
-  rake "waves:close_terminated_vessels"
+every 1.day, "daily_jobs", at: "04:00" do
+  DailyJob.all
+end
+
+class DailyJob
+  class << self
+    def all
+      unclaim_claimed_submissions
+      expire_referrals
+      process_reminders
+    end
+
+    private
+
+    def unclaim_claimed_submissions
+      Submission.assigned.map(&:unclaimed!)
+    end
+
+    def expire_referrals
+      Submission.referred.referred_until_expired.each(&:unreferred!)
+    end
+
+    def process_reminders
+      CarvingAndMarkingReminder.process
+      CodeCertificateReminder.process
+      RegistrationRenewalReminder.process
+      SafetyCertificateReminder.process
+    end
+  end
 end
