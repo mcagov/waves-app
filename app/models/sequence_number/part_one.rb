65450bd1a21ea53f195995d219a8c2eed733d42d
Toby Privett <toby@snaplab.co.uk>
2018-06-15 15:09:15 +0100

    Update part one official number to start at 2000000

--- app/models/sequence_number/part_one.rb
+++ app/models/sequence_number/part_one.rb
@@ -1,5 +1,5 @@
 class SequenceNumber::PartOne < SequenceNumber::Generator
   protokoll :generated_number,
-            start: 930000,
+            start: 2000000,
             pattern: SequenceNumber::Generator::REG_NO_PATTERNS[:part_1]
 end
