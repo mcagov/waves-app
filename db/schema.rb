4d8431f048323287b091ca8505ce4edae2b4a39f
Toby Privett <toby@snaplab.co.uk>
2018-07-19 10:00:25 +0100

    Add start_date to submission_tasks
    - Prefer date type over datetime. This might involve
    less date parsing when calculating target dates and
    service standards.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_16_130615) do
+ActiveRecord::Schema.define(version: 2018_07_19_084741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -409,9 +409,10 @@ ActiveRecord::Schema.define(version: 2018_07_16_130615) do
     t.datetime "updated_at", null: false
     t.integer "price", default: 0
     t.integer "submission_ref_counter"
-    t.datetime "target_date"
+    t.date "target_date"
     t.string "state"
     t.integer "service_level", default: 0
+    t.date "start_date"
     t.index ["claimant_id"], name: "index_submission_tasks_on_claimant_id"
     t.index ["service_id"], name: "index_submission_tasks_on_service_id"
     t.index ["submission_id"], name: "index_submission_tasks_on_submission_id"
