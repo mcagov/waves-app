8fcacf0de62d55be05d8af7b5dc84b5d3fc5c842
Toby Privett <toby@snaplab.co.uk>
2018-08-09 11:11:29 +0100

    Implement spec: user views part_3 submission details

--- spec/features/part_3/user_views_submission_details_spec.rb
+++ spec/features/part_3/user_views_submission_details_spec.rb
@@ -1,7 +1,13 @@
 require "rails_helper"
 
-xfeature "User views owner submission details", type: :feature, js: true do
-  let!(:submission) { create(:unassigned_change_vessel_submission) }
+feature "User views part_3 submission details", type: :feature, js: true do
+  let!(:submission) do
+    create(:claimed_task,
+           submission: create(:submission, :part_3_vessel),
+           service: create(:service, :update_registry_details)
+          ).submission
+  end
+
   let!(:registered_vessel) { submission.registered_vessel }
   let!(:registered_owner) { registered_vessel.owners.first }
   let!(:declaration) { submission.declarations.first }
@@ -14,7 +20,7 @@ xfeature "User views owner submission details", type: :feature, js: true do
     visit submission_path(submission)
   end
 
-  scenario "displaying the submission and registry owner lists" do
+  scenario "owner tab" do
     owner = declaration.owner
 
     click_on("Owners")
@@ -22,11 +28,29 @@ xfeature "User views owner submission details", type: :feature, js: true do
     expect(page).to have_css(".owner-email", text: owner.email)
     expect(page).to have_css(".owner-phone_number", text: owner.phone_number)
     expect(page).to have_css(".owner-nationality", text: owner.nationality)
-    expect(page).to have_css(".declaration-status", text: "n/a")
+    expect(page).to have_button("Complete Declaration")
 
     expect(page).to have_css(".strike", text: registered_owner.name)
     expect(page).to have_css(".strike", text: registered_owner.email)
     expect(page).to have_css(".strike", text: registered_owner.nationality)
     expect(page).to have_css(".strike", text: registered_owner.inline_address)
+  end
+
+  scenario "vessel tab" do
+    click_on("Vessel Info")
+    within("table.submission-vessel") do
+      expect(page).to have_css("th", count: 3)
+      expect(page)
+        .to have_css("td#registry-vessel-name", text: registered_vessel.name)
+    end
+  end
+
+  scenario "agent tab" do
+    click_on("Agent")
+    within("table#agent") do
+      expect(page)
+        .to have_css(
+          ".registry-agent-name", text: registered_vessel.agent.name)
+    end
   end
 end
