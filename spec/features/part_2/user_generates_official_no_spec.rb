ee944e1662f148356ad998ca2f896a05d6f9c5b1
Toby Privett <toby@snaplab.co.uk>
2018-08-14 14:35:00 +0100

    Fix generate official no spec

--- spec/features/part_2/user_generates_official_no_spec.rb
+++ spec/features/part_2/user_generates_official_no_spec.rb
@@ -1,8 +1,10 @@
 require "rails_helper"
 
-xdescribe "User generates an official no", js: true do
+describe "User generates an official no", js: true do
   before do
-    visit_name_approved_part_2_submission
+    # with a submission that has a name approval
+    visit_claimed_task(
+      submission: create(:name_approval).submission)
 
     within("#heading .official-no") do
       click_on("Generate")
