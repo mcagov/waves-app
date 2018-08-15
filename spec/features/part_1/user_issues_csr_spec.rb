559059898316b8a547836a7beb6b27ba2884912b
Toby Privett <toby@snaplab.co.uk>
2018-08-15 15:35:12 +0100

    Save Details when the Complete Task button is clicked
    - This is proving hard to test. Not keen to start passing around parameters that will
    enforce different workflows
    
    - For now, marking the intended spec as pending

--- spec/features/part_1/user_issues_csr_spec.rb
+++ spec/features/part_1/user_issues_csr_spec.rb
@@ -1,11 +1,13 @@
 require "rails_helper"
 
 describe "User issues CSR", js: true do
-  scenario "in general" do
+  before do
     visit_claimed_task(
       submission: create(:submission, :part_1_vessel),
       service: create(:demo_service, :issues_csr))
+  end
 
+  scenario "save details with the Save Details button" do
     expect(find("#csr_form_issue_number").value).to eq("1")
     expect(find("#csr_form_vessel_name").value).to match(/Registered Boat.*/)
     find("#csr_form_remarks").set("NO COMMENT")
@@ -17,9 +19,17 @@ describe "User issues CSR", js: true do
     expect(Submission.last.csr_form.remarks).to eq("NO COMMENT")
   end
 
+  xscenario "save details with the Complete Task button" do
+    find("#csr_form_remarks").set("NO COMMENT")
+    click_on("Complete Task")
+
+    expect(page).to have_css(".modal-title", text: "Task: Demo Service")
+    expect(Submission.last.csr_form.remarks).to eq("NO COMMENT")
+  end
+
   xscenario "pending implementation" do
-    click_on("Issue CSR")
-    click_on("Approve & Issue CSR")
+    click_on("Complete Task")
+    within("modal.fade.in") { click_on("Complete Task") }
 
     pdf_window = window_opened_by do
       click_on("Print CSR")
