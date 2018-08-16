39025822ab743e113d6262eaa3479c20796e16a6
Toby Privett <toby@snaplab.co.uk>
2018-08-16 13:33:50 +0100

    Rewrite the closed registration builder

--- spec/models/builders/closed_registration_builder_spec.rb
+++ spec/models/builders/closed_registration_builder_spec.rb
@@ -2,37 +2,41 @@ require "rails_helper"
 
 describe Builders::ClosedRegistrationBuilder do
   context ".create" do
-    before do
+    let(:task) do
+      create(:task,
+             submission: create(:submission, :part_3_vessel))
+    end
+
+    subject do
       described_class.create(
-        submission,
+        task,
         "10/10/2012 12:23 PM".to_datetime,
         "Mi Razon",
         "additional details")
     end
 
-    let!(:registered_vessel) { create(:registered_vessel) }
-
-    let!(:submission) do
-      create(:submission,
-             registered_vessel: registered_vessel, application_type: :closure)
-    end
-
-    let(:registration) { registered_vessel.reload.current_registration }
-
     it "records the closed_at date" do
-      expect(registration.closed_at).to eq("10/10/2012 12:23 PM".to_datetime)
+      expect(subject.closed_at).to eq("10/10/2012 12:23 PM".to_datetime)
     end
 
     it "records the closure reason" do
-      expect(registration.description).to eq("Mi Razon")
+      expect(subject.description).to eq("Mi Razon")
     end
 
     it "records the supporting_info" do
-      expect(registration.supporting_info).to eq("additional details")
+      expect(subject.supporting_info).to eq("additional details")
     end
 
     it "records the registration#actioned_by" do
-      expect(registration.actioned_by).to eq(submission.claimant)
+      expect(subject.actioned_by).to eq(task.claimant)
+    end
+
+    it "assigns the submission#registration" do
+      expect(subject).to eq(Submission.last.registration)
+    end
+
+    it "assigns the registered_vessel#current_registration" do
+      expect(subject).to eq(Register::Vessel.last.current_registration)
     end
   end
 end
