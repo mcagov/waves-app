0751694d23bce00b995259ff9adb61e835e39872
Toby Privett <toby@snaplab.co.uk>
2018-08-21 10:03:28 +0100

    Fix spec: report carving and marking

--- spec/features/part_2/user_reports_carving_and_marking_spec.rb
+++ spec/features/part_2/user_reports_carving_and_marking_spec.rb
@@ -1,8 +1,11 @@
 require "rails_helper"
 
-xdescribe "User reports receipt of a Carving & Marking Note", js: true do
+describe "User reports receipt of a Carving & Marking Note", js: true do
   scenario "in general" do
-    visit_carving_and_marking_ready_submission
+    visit_claimed_task(
+      submission: create(:name_approval).submission,
+      service: create(:demo_service, :update_registry_details))
+
     click_on("Certificates & Documents")
 
     within("#carving_and_marking .status") do
@@ -11,6 +14,10 @@ xdescribe "User reports receipt of a Carving & Marking Note", js: true do
 
     within("#carving_and_marking .status") do
       click_on("Mark as Pending")
+    end
+
+    within("#carving_and_marking .status") do
+      expect(page).to have_link("Mark as Received")
     end
   end
 end
