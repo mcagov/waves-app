a0522bc68d857715655d3ea570777a45e6b5a22a
Toby Privett <toby@snaplab.co.uk>
2018-08-28 10:29:56 +0100

    Index services#name column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_23_140502) do
+ActiveRecord::Schema.define(version: 2018_08_28_091913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -387,6 +387,7 @@ ActiveRecord::Schema.define(version: 2018_08_23_140502) do
     t.json "rules", default: []
     t.json "activities", default: []
     t.json "print_templates", default: []
+    t.index ["name"], name: "index_services_on_name"
   end
 
   create_table "shareholder_group_members", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
