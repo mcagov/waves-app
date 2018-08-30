512f752026df4664d557fc958f0b764c852943f9
Toby Privett <toby@snaplab.co.uk>
2018-08-30 08:40:59 +0100

    Remove Advanced Search
    - This was built quickly and poorly. Under duress from a previous PM
    - It doesn't do what RSS want

--- spec/features/access_levels/team_leader_spec.rb
+++ spec/features/access_levels/team_leader_spec.rb
@@ -10,12 +10,6 @@ describe "Team leader" do
     expect(page).to have_link("Finance Team")
   end
 
-  scenario "management reports" do
-    click_on("Management Reports")
-
-    expect(page).not_to have_link("Advanced Search")
-  end
-
   scenario "admin tools" do
     click_on("Part 3:")
 
