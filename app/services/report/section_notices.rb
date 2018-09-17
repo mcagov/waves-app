bc4a5c962e843e63019bd9dca768849b37c5f5da
Toby Privett <toby@snaplab.co.uk>
2018-09-17 15:05:02 +0100

    Update Section Notice report

--- app/services/report/section_notices.rb
+++ app/services/report/section_notices.rb
@@ -7,16 +7,14 @@ class Report::SectionNotices < Report
     [
       "Name", "Part", "Official No", "Section Notice Issue Date",
       "Section Notice Issue Date + 30", "Regulation Reference",
-      "Termination Notice Issue Date", "Status"
+      "Current Registration Status"
     ]
   end
 
   def results # rubocop:disable Metrics/MethodLength
-    @pagination_collection = vessels
-    @pagination_collection.map do |vessel|
-      section_notice = vessel.section_notices.first
-      section_notice ||= Register::SectionNotice.new
-
+    @pagination_collection = section_notices
+    @pagination_collection.map do |section_notice|
+      vessel = section_notice.vessel
       Result.new(
         [
           RenderAsLinkToVessel.new(vessel, :name),
@@ -25,17 +23,14 @@ class Report::SectionNotices < Report
           section_notice.created_at,
           date_plus_30(section_notice.created_at),
           section_notice.regulation_key,
-          termination_issue_date(section_notice, vessel),
           RenderAsRegistrationStatus.new(vessel.registration_status),
         ])
     end
   end
 
-  def vessels
-    query = Register::Vessel.frozen
-    query = query.where(
-      "state in (?)", %w(section_notice_issued termination_notice_issued))
-    paginate(query.includes(:section_notices).order(:name))
+  def section_notices
+    query = Register::SectionNotice.includes(:noteable).order(created_at: :desc)
+    paginate(query)
   end
 
   private
