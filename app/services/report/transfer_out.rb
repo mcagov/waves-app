4db7c5b48ac764760a7f7f66ace5a428f4a6a230
Toby Privett <toby@snaplab.co.uk>
2018-09-21 11:00:11 +0100

    User view report: Transfers Out

---
+++ app/services/report/transfer_out.rb
@@ -0,0 +1,42 @@
+class Report::TransferOut < Report
+  def title
+    "Transfers Out"
+  end
+
+  def filter_fields
+    [:filter_part, :filter_date_range]
+  end
+
+  def date_range_label
+    "Date Registration Closed"
+  end
+
+  def headings
+    ["Name", "Part", "IMO Number", "Gross Tonnage", "Date Registration Closed"]
+  end
+
+  def results
+    return [] unless @date_start && @date_end
+    vessels.map do |vessel|
+      Result.new(
+        [
+          RenderAsLinkToVessel.new(vessel, :name),
+          vessel.part.to_s.humanize,
+          vessel.imo_number,
+          vessel.gross_tonnage,
+          vessel.current_registration.try(:closed_at)])
+    end
+  end
+
+  private
+
+  def vessels
+    Register::Vessel
+      .joins(:current_registration)
+      .in_part(@part)
+      .where("registrations.closed_at >= ? ", @date_start)
+      .where("registrations.closed_at <= ? ", @date_end)
+      .order("registrations.closed_at desc")
+      .all
+  end
+end
