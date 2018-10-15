8560c9361d123ed8d3379610913410047e0231a8
Toby Privett <toby@snaplab.co.uk>
2018-10-15 15:20:59 +0100

    Fix bugs with AgentBuilder#address and display agent address on vessel page

--- app/models/builders/registry/agent_builder.rb
+++ app/models/builders/registry/agent_builder.rb
@@ -11,12 +11,18 @@ class Builders::Registry::AgentBuilder
 
     private
 
-    def perform
+    def perform # rubocop:disable all
       if @submission.agent
         agent = @vessel.agent || @vessel.build_agent
         agent.name = @submission.agent.name
         agent.email = @submission.agent.email
         agent.phone_number = @submission.agent.phone_number
+        agent.address_1 = @submission.agent.address_1
+        agent.address_2 = @submission.agent.address_2
+        agent.address_3 = @submission.agent.address_3
+        agent.town = @submission.agent.town
+        agent.postcode = @submission.agent.postcode
+        agent.country = @submission.agent.country
         agent.save
       end
     end
