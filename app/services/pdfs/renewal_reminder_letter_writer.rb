a7c485e7aab8c4c2342864bb590a469371d1bfa7
Toby Privett <toby@snaplab.co.uk>
2018-10-15 09:22:19 +0100

    Ensure renewal reminder recipient is derived from vessel details

--- app/services/pdfs/renewal_reminder_letter_writer.rb
+++ app/services/pdfs/renewal_reminder_letter_writer.rb
@@ -5,8 +5,9 @@ class Pdfs::RenewalReminderLetterWriter
     @registration = registration
     @vessel = @registration.registered_vessel
     @applicant_name = @vessel.correspondent
-    @delivery_name_and_address = @registration.delivery_name_and_address
-
+    @applicant_name = @vessel.correspondent.name
+    address = @vessel.correspondent.try(:compacted_address)
+    @delivery_name_and_address = [@applicant_name] + address
     @pdf = pdf
   end
 end
