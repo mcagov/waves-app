770f95fc77076513ce1cd1f39fe8fb8ef23afa0e
Toby Privett <toby@snaplab.co.uk>
2019-03-12 10:44:38 +0000

    Ensure that carving_and_marking email has attachment

--- app/models/notification/carving_and_marking_note.rb
+++ app/models/notification/carving_and_marking_note.rb
@@ -4,7 +4,7 @@ class Notification::CarvingAndMarkingNote < Notification
   end
 
   def additional_params
-    [body, actioned_by]
+    [body, actioned_by, email_attachments]
   end
 
   def email_subject
@@ -15,5 +15,11 @@ class Notification::CarvingAndMarkingNote < Notification
 
   def vessel_name
     notifiable.vessel_name if notifiable
+  end
+
+  def email_attachments
+    Array(attachments).map do |attachment|
+      Pdfs::Processor.run(attachment.to_sym, notifiable, :attachment).render
+    end
   end
 end
